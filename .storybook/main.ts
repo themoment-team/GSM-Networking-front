7b0acb42e406d9ad873ca4a1b38287794ff44a23
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 15:55:51 +0900

    Add storybook setting

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-styling',
+    '@storybook/addon-viewport/register',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {
+      nextConfigPath: '../next.config.js',
+    },
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path?.resolve(__dirname, '../src/');
+    return config;
+  },
+};
+export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { GlobalStyle, theme } from '../src/styles';
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  width600: {
+    name: 'width 600',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  ...INITIAL_VIEWPORTS,
+};
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+      backgrounds: {
+        default: 'default',
+        values: [
+          {
+            name: 'default',
+            value: '#F5F6F8',
+          },
+          {
+            name: 'light',
+            value: '#ffffff',
+          },
+          {
+            name: 'dark',
+            value: '#050505',
+          },
+        ],
+      },
+      nextjs: {
+        appDirectory: true,
+      },
+    },
+    viewport: {
+      viewports: customViewports,
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
