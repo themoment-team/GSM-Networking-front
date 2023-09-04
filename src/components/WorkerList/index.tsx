4634c5026c5e653802e9e2c7707b3d598770bf3f
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 19:01:15 +0900

    Update random sorting

--- src/components/WorkerList/index.tsx
+++ src/components/WorkerList/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import * as S from './style';
 
 import { WorkerListItem } from '@/components';
@@ -9,14 +11,22 @@ interface Props {
   workerList: WorkerType[];
 }
 
-const WorkerList: React.FC<Props> = ({ workerList }) => (
-  <S.WorkerList>
-    {workerList
-      .sort(() => Math.random() - 0.5)
-      .map((worker) => (
-        <WorkerListItem key={worker.id} worker={worker} />
-      ))}
-  </S.WorkerList>
-);
+const WorkerList: React.FC<Props> = ({ workerList: workerListProp }) => {
+  const [workerList, setWorkerList] = useState<WorkerType[]>(workerListProp);
+
+  useEffect(() => {
+    setWorkerList(workerListProp.sort(() => Math.random() - 0.5));
+  }, [workerListProp]);
+
+  return (
+    <S.WorkerList>
+      {workerList
+        .sort(() => Math.random() - 0.5)
+        .map((worker) => (
+          <WorkerListItem key={worker.id} worker={worker} />
+        ))}
+    </S.WorkerList>
+  );
+};
 
 export default WorkerList;
