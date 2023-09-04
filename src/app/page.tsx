96a1ab1d36c419cfe0d636b315e597949906a1fa
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 16:48:37 +0900

    Add Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Header, WorkerList } from '@/components';
+import type { WorkerType } from '@/types';
 
-export default function Home() {
+import type { Metadata } from 'next';
+
+export default async function Home() {
+  const workerList: WorkerType[] = await fetch(
+    `${process.env.BASE_URL}/api/worker/list`,
+    {
+      cache: 'force-cache',
+    }
+  ).then((res) => res.json());
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <Header />
+      <WorkerList workerList={workerList.sort(() => Math.random() - 0.5)} />
+    </>
+  );
 }
+
+export const metadata: Metadata = {
+  title: '취업자 리스트 조회',
+  description:
+    '광주소프트웨어마이스터고등학교 학생들의 취업 정보를 확인 할 수 있어요.',
+  openGraph: {
+    title: '취업자 리스트 조회',
+    description:
+      '광주소프트웨어마이스터고등학교 학생들의 취업 정보를 확인 할 수 있어요.',
+  },
+};
