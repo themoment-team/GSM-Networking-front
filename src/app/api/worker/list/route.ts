8c60b91f4c56a5f1fecb9abe86c9e14133146567
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 16:48:01 +0900

    Add /api/worker/list

---
+++ src/app/api/worker/list/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+import getWorkerList from '@/apis/getWorkerList';
+
+export async function GET() {
+  const workerList = await getWorkerList();
+
+  return NextResponse.json(workerList);
+}
