0cba7cccadabb554379012a62d9b94f7b4096503
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 16:38:29 +0900

    Add Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import * as S from './style';
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <S.Layout>{children}</S.Layout>
+);
+
+export default Layout;
---
+++ src/components/Layout/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Layout = styled.main`
+  width: 600px;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    padding: 0 1rem;
+  }
+`;
