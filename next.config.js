d4244b432f0df4c17ce0c2ed5dcc8b0d48f0beb4
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-04 15:40:50 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
