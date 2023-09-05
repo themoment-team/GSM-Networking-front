d74cd12db510154a6ae585be073de3ad35645cff
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-05 10:10:17 +0900

    Add Icons

---
+++ src/assets/FilterIcon.tsx
@@ -0,0 +1,37 @@
+const FilterIcon = () => (
+  <svg
+    width='1rem'
+    height='1rem'
+    viewBox='0 0 16 16'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <g id='Frame 39'>
+      <path
+        id='Vector 5'
+        d='M12.6608 2H3.33921C2.24005 2 1.61059 3.24951 2.26663 4.12914L6.40245 9.64573C6.57401 9.87577 6.66667 10.1548 6.66667 10.4415V12.6667C6.66667 13.403 7.26171 14 8 14C8.73829 14 9.31879 13.403 9.31879 12.6667V10.4444C9.31879 10.156 9.41261 9.87524 9.58615 9.64444L13.7302 4.13333C14.3912 3.25435 13.7624 2 12.6608 2Z'
+        fill='#148EFF'
+      />
+      <rect
+        id='Rectangle 26'
+        x='2'
+        y='10'
+        width='2.66667'
+        height='4'
+        rx='0.666667'
+        fill='#94CCFF'
+      />
+      <rect
+        id='Rectangle 27'
+        x='11.3333'
+        y='10'
+        width='2.66667'
+        height='4'
+        rx='0.666667'
+        fill='#94CCFF'
+      />
+    </g>
+  </svg>
+);
+
+export default FilterIcon;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
+export { default as CloseIcon } from './CloseIcon';
 export { default as EmailIcon } from './EmailIcon';
+export { default as FilterIcon } from './FilterIcon';
 export { default as SNSIcon } from './SNSIcon';
