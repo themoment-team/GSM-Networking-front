1b0044dbc0683d1f807181832c4e2c4d38e8c261
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-05 15:16:50 +0900

    Update mobile view layout

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export const Layout = styled.main`
   width: 600px;
-  height: 100vh;
-  height: 100dvh;
   margin: 0 auto;
+  padding-top: 5rem;
+  position: relative;
 
   @media (max-width: 600px) {
     width: 100%;
-    padding: 0 1rem;
+    padding: 5rem 1rem 0 1rem;
   }
 `;
