6a6a504c38a02478b19a62da08f228bd295e8039
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-05 14:59:22 +0900

    Add export files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
+export * from './Buttons';
+export * from './Modal';
+export { default as FilterButton } from './Buttons/Filter';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as SearchBar } from './SearchBar';
-// export { default as RegisterForm } from './RegisterForm';
 export { default as WorkerList } from './WorkerList';
 export { default as WorkerListItem } from './WorkerListItem';
+// export { default as RegisterForm } from './RegisterForm';
