fb83ce7e6cfbdb6a2c8ff08382fd49787cb279e7
신희성 <131235625+tlsgmltjd@users.noreply.github.com>
2023-09-05 10:10:16 +0900

    Update src/components/index.ts
    Co-authored-by: 김형록 <khr5k0228@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
-
 export { default as SearchBar } from './SearchBar';
 // export { default as RegisterForm } from './RegisterForm';
 export { default as WorkerList } from './WorkerList';
