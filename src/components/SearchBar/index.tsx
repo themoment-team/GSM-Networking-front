6f2210c2445c9a82a21d3953d78c095b5f0683c8
신희성 <vvwv3092@gmail.com>
2023-09-05 10:26:36 +0900

    Update Search system

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { Dispatch, SetStateAction } from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import * as S from './style';
 
@@ -16,23 +16,24 @@ interface Props {
 const SearchBar: React.FC<Props> = ({ initWorkerList, setWorkerList }) => {
   const [searchValue, setSeaechValue] = useState<string>('');
 
-  useEffect(() => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+
     const searched = initWorkerList.filter(
       (worker) =>
-        worker.name.includes(searchValue) ||
-        worker.company.name.includes(searchValue) ||
-        worker.position.includes(searchValue)
+        worker.name.includes(inputValue) ||
+        worker.company.name.includes(inputValue) ||
+        worker.position.includes(inputValue)
     );
-    setWorkerList(searched);
-  }, [initWorkerList, searchValue, setWorkerList]);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSeaechValue(e.currentTarget.value);
+    setSeaechValue(inputValue);
+    setWorkerList(searched);
   };
 
   return (
     <S.SearchBarContainer>
       <S.SearchInput
+        value={searchValue}
         onChange={onChange}
         placeholder='찾고 싶은 사람, 전공, 회사명, 기수 등을 검색해보세요'
       />
