ee9bb64b6eedacad5df06db1ef54e906b68c6df6
신희성 <vvwv3092@gmail.com>
2023-09-07 08:35:39 +0900

    Add LogoIcon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 export { default as CloseIcon } from './CloseIcon';
 export { default as EmailIcon } from './EmailIcon';
 export { default as FilterIcon } from './FilterIcon';
+export { default as LogoIcon } from './LogoIcon';
 export { default as SNSIcon } from './SNSIcon';
 export { default as SearchIcon } from './SearchIcon';
