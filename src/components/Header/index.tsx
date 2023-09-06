31a3cd62e8570f65146ed48ccb609713dc114772
신희성 <vvwv3092@gmail.com>
2023-09-07 08:36:45 +0900

    Add LogoIcon place

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,12 @@
 
 import * as S from './style';
 
+import { LogoIcon } from '@/assets';
+
 const Header = () => (
   <S.Header>
     <S.Inner>
+      <LogoIcon />
       <S.PageName>취업자 리스트 조회</S.PageName>
     </S.Inner>
   </S.Header>
