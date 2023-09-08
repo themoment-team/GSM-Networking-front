9138a91de04006f1349a8dc143c11c77efbf672e
gaoooon <gaon9749@gmail.com>
2023-09-08 13:14:32 +0900

    Update DropDown component

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,14 +1,24 @@
-const DropDown = () => {
+'use client';
+
+import * as S from './style';
+
+const DropDown = ({}) => {
   return (
-    <div>
-      <ul>
-        <li>
-          <a href='https://gsm-networking.notion.site/GSM-Networking-06e11dfad2c34be18559fa0c0b1f8573?pvs=4'>
-            개인정보 처리방침
-          </a>
-        </li>
-      </ul>
-    </div>
+    <S.Opacity
+      onClick={(e) => {
+        console.log(e);
+      }}
+    >
+      <S.Container>
+        <S.ListContainer>
+          <S.ListItem>
+            <S.LinkItem href='https://gsm-networking.notion.site/GSM-Networking-06e11dfad2c34be18559fa0c0b1f8573?pvs=4'>
+              개인정보 처리방침
+            </S.LinkItem>
+          </S.ListItem>
+        </S.ListContainer>
+      </S.Container>
+    </S.Opacity>
   );
 };
 
