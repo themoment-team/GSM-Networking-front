6b2a8d09217fe32fdc0848440d020c0d3bc29068
gaoooon <gaon9749@gmail.com>
2023-09-08 13:11:42 +0900

    Update style.ts

--- src/components/DropDown/style.ts
+++ src/components/DropDown/style.ts
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
+export const Opacity = styled.div`
+  background: rgba(0, 0, 0, 0.25);
+  height: 100vh;
+  position: fixed;
+  top: 80px;
+  z-index: 10;
+`;
+
 export const Container = styled.div`
   height: 160px;
-  background-color: aliceblue;
-  border-radius: 0 0 30px 30px;
+  background-color: #ffffff;
+  border-radius: 0 0 10px 10px;
+  width: 100vw;
+`;
+
+export const ListContainer = styled.ul`
+  margin: 0 auto;
+  width: 600px;
+  height: 160px;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 10px 20px;
+  align-items: start;
+  flex-wrap: wrap;
+`;
+
+export const ListItem = styled.li``;
+
+export const LinkItem = styled.a`
+  font-weight: 500;
+  font-size: 16px;
+  color: #828387;
 `;
