3f25f85cfc152d5e9139db6e778407b8cd094a74
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-08 18:06:46 +0900

    Resolve client error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
+
 export function middleware(request: NextRequest) {
   if (!request.nextUrl.pathname.startsWith('/inspection')) {
     return NextResponse.redirect(new URL('/inspection', request.url));
