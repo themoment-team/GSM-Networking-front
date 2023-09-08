337d84cf2f9f0c33cd5ef75070dba5563edd572c
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-08 16:10:31 +0900

    Add NavigationEvents

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from './Modal';
 export { default as FilterButton } from './Buttons/Filter';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
+export { default as NavigationEvents } from './NavigationEvents';
 export { default as SearchBar } from './SearchBar';
 export { default as WorkerList } from './WorkerList';
 export { default as WorkerListItem } from './WorkerListItem';
