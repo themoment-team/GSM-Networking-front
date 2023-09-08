55ac73d5c4bd16833760481134b45352165739ba
gaoooon <gaon9749@gmail.com>
2023-09-08 23:32:38 +0900

    Update media

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,4 +1,6 @@
 'use client';
+import Link from 'next/link';
+
 import * as S from './style';
 
 import * as I from '@/assets';
@@ -7,10 +9,17 @@ const SignIn = () => (
   <S.Container>
     <S.LogoContainer>
       <I.LoginLogoIcon />
+      <I.SmallLoginLogo />
     </S.LogoContainer>
-    <S.LoginButton>
-      <I.LoginButton />
-    </S.LoginButton>
+    <Link
+      href={
+        'https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=807267617496-dvq6b3b9phu5p8pu2k43iakec6jb1149.apps.googleusercontent.com&redirect_uri=https://gsm.moip.shop/auth/signin/google&response_type=code&scope=email%20profile%20openid&access_type=offline&service=lso&o2v=1&flowName=GeneralOAuthFlow'
+      }
+    >
+      <S.LoginButton>
+        <I.LoginButton />
+      </S.LoginButton>
+    </Link>
     <S.TextContainer>*GSM 계정으로만 접속 가능합니다.</S.TextContainer>
   </S.Container>
 );
--- src/components/SignIn/style.ts
+++ src/components/SignIn/style.ts
@@ -8,6 +8,29 @@ export const Container = styled.div`
 
 export const LogoContainer = styled.div`
   margin-top: 356px;
+  font-weight: 700;
+  font-family: 'LINE Seed Sans KR';
+  font-size: 96px;
+
+  & > svg:last-child {
+    display: none;
+  }
+
+  & > svg:first-child {
+    display: block;
+  }
+
+  @media (max-width: 600px) {
+    margin-top: 10rem;
+
+    & > svg:last-child {
+      display: block;
+    }
+
+    & > svg:first-child {
+      display: none;
+    }
+  }
 `;
 
 export const LoginButton = styled.button`
