aef7633f167be93dca181bc228f9ff7567e08c57
gaoooon <gaon9749@gmail.com>
2023-09-08 11:05:42 +0900

    Add DropDown component

---
+++ src/components/DropDown/index.tsx
@@ -0,0 +1,15 @@
+const DropDown = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href='https://gsm-networking.notion.site/GSM-Networking-06e11dfad2c34be18559fa0c0b1f8573?pvs=4'>
+            개인정보 처리방침
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropDown;
