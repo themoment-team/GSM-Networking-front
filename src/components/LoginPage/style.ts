ed4fa9872fd75d12bc234048f6d3c467b1a33167
gaoooon <gaon9749@gmail.com>
2023-09-08 18:14:58 +0900

    Add styled component

---
+++ src/components/LoginPage/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LogoContainer = styled.div`
+  margin-top: 356px;
+`;
+
+export const LoginButton = styled.button`
+  margin: 70px 0 30px 0;
+`;
+
+export const TextContainer = styled.p`
+  color: #828387;
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: 600;
+`;
