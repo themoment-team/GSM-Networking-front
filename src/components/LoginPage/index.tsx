2c75483f213988741237780adaad601d68e44e9e
gaoooon <gaon9749@gmail.com>
2023-09-08 18:14:42 +0900

    Add LoginPage component

---
+++ src/components/LoginPage/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+import * as S from './style';
+import * as I from '@/assets';
+
+const LoginPage = () => {
+  return (
+    <S.Container>
+      <S.LogoContainer>
+        <I.LoginLogoIcon />
+      </S.LogoContainer>
+      <S.LoginButton>
+        <I.LoginButton />
+      </S.LoginButton>
+      <S.TextContainer>*GSM 계정으로만 접속 가능합니다.</S.TextContainer>
+    </S.Container>
+  );
+};
+
+export default LoginPage;
