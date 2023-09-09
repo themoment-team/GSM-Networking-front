2bd60d41cbd7e120b7412580225b813caf5cac5f
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-09 15:14:10 +0900

    Update assets

--- src/assets/MoreIcon.tsx
+++ src/assets/MoreIcon.tsx
@@ -1,7 +1,7 @@
 const MoreIcon = () => (
   <svg
-    width='24'
-    height='24'
+    width='1.5rem'
+    height='1.5rem'
     viewBox='0 0 24 24'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
