3686bd0c5b5a2aae78efd014869cd182ea6dd3c3
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-09 15:14:40 +0900

    Add signin metadata

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,16 @@
 import { SignIn } from '@/components';
 
+import type { Metadata } from 'next';
+
 export default function SignInPage() {
   return <SignIn />;
 }
+
+export const metadata: Metadata = {
+  title: '로그인',
+  description: '로그인 페이지 입니다.',
+  openGraph: {
+    title: '로그인',
+    description: '로그인 페이지 입니다.',
+  },
+};
