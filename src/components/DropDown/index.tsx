260368cbabf3e5f79e49330569397f82e25f2e6c
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-12 07:59:00 +0900

    Add about team url

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -5,6 +5,9 @@ import * as S from './style';
 const policyUrl =
   'https://gsm-networking.notion.site/GSM-Networking-06e11dfad2c34be18559fa0c0b1f8573?pvs=4';
 
+const teamUrl =
+  'https://gsm-networking.notion.site/GSM-Networking-30599e51feb345a781d1f41be1962f31?pvs=4';
+
 const DropDown = () => (
   <S.Opacity>
     <S.Container>
@@ -16,6 +19,11 @@ const DropDown = () => (
             rel='noopener noreferrer'
           >
             개인정보 처리방침
+          </S.LinkItem>
+        </S.ListItem>
+        <S.ListItem>
+          <S.LinkItem href={teamUrl} target='_blank' rel='noopener noreferrer'>
+            About Team
           </S.LinkItem>
         </S.ListItem>
       </S.ListContainer>
