a25aafad17d75a88a6bf9bbad1ec761aba449f45
gaoooon <gaon9749@gmail.com>
2023-09-11 23:22:57 +0900

    Update style.ts

--- src/components/DropDown/style.ts
+++ src/components/DropDown/style.ts
@@ -2,15 +2,10 @@ import styled from 'styled-components';
 
 export const Opacity = styled.div`
   background: rgba(0, 0, 0, 0.25);
-  height: 100vh;
-  height: 100dvh;
-  position: fixed;
-  left: 0;
-  top: 5rem;
-  z-index: 10;
+  height: 77.7vh;
 `;
 
-export const Container = styled.div`
+export const MenuContainer = styled.div`
   width: 100vw;
   height: 10rem;
   background-color: ${({ theme }) => theme.color.white};
@@ -38,4 +33,11 @@ export const ListItem = styled.li``;
 export const LinkItem = styled.a`
   ${({ theme }) => theme.typo.body1};
   color: ${({ theme }) => theme.color.grey[600]};
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  left: 0;
+  top: 5rem;
+  z-index: 10;
 `;
