7c3c7296798753e408ba670abd6edc5b95481520
gaoooon <gaon9749@gmail.com>
2023-09-12 16:28:42 +0900

    Add stopPropagation

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React from 'react';
 
 import * as S from './style';
@@ -10,31 +11,22 @@ interface DropDownProps {
 const policyUrl =
   'https://gsm-networking.notion.site/GSM-Networking-06e11dfad2c34be18559fa0c0b1f8573?pvs=4';
 
-const DropDown: React.FC<DropDownProps> = ({ setIsDrop }) => {
-  const backgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-    setIsDrop(false);
-    return false;
-  };
-
-  return (
-    <S.Container>
-      <S.MenuContainer>
-        <S.ListContainer>
-          <S.ListItem>
-            <S.LinkItem
-              href={policyUrl}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              개인정보 처리방침
-            </S.LinkItem>
-          </S.ListItem>
-        </S.ListContainer>
-      </S.MenuContainer>
-      <S.Opacity onClick={backgroundClick} />
+const DropDown: React.FC<DropDownProps> = ({ setIsDrop }) => (
+  <S.Opacity onClick={() => setIsDrop(false)}>
+    <S.Container onClick={(e) => e.stopPropagation()}>
+      <S.ListContainer>
+        <S.ListItem>
+          <S.LinkItem
+            href={policyUrl}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            개인정보 처리방침
+          </S.LinkItem>
+        </S.ListItem>
+      </S.ListContainer>
     </S.Container>
-  );
-};
+  </S.Opacity>
+);
 
 export default DropDown;
