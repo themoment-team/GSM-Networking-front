9833124c89b022c39a70698a2c94718c48661a5b
방가온 <128475660+gaoooon@users.noreply.github.com>
2023-09-14 14:56:22 +0900

    Update src/assets/GsmNetworkingIcon.tsx
    Co-authored-by: 김형록 <khr5k0228@gmail.com>

--- src/assets/GsmNetworkingIcon.tsx
+++ src/assets/GsmNetworkingIcon.tsx
@@ -1,7 +1,7 @@
 const GsmNetworkingIcon = () => (
   <svg
-    width='24'
-    height='21'
+    width='1.5rem'
+    height='1.3125rem'
     viewBox='0 0 24 21'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
