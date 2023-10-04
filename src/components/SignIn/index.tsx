23828bf760d0ebe5099dd8a2ea14fd1f23a8229a
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-04 16:51:32 +0900

    Update oauthLink

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -9,7 +9,7 @@ import * as S from './style';
 import { GoogleIcon } from '@/assets';
 
 const oauthLink =
-  'https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=807267617496-dvq6b3b9phu5p8pu2k43iakec6jb1149.apps.googleusercontent.com&redirect_uri=https://gsm.moip.shop/auth/signin/google&response_type=code&scope=email%20profile%20openid&access_type=offline&service=lso&o2v=1&flowName=GeneralOAuthFlow';
+  'https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=807267617496-dvq6b3b9phu5p8pu2k43iakec6jb1149.apps.googleusercontent.com&redirect_uri=https://gsm-networking.com/auth/signin/google&response_type=code&scope=email%20profile%20openid&access_type=offline&service=lso&o2v=1&flowName=GeneralOAuthFlow';
 
 const SignIn = () => {
   const searchParams = useSearchParams();
