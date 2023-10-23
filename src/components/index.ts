421629d4b118b1e6f27b7a182be5b785166c080b
gaoooon <gaon9749@gmail.com>
2023-10-23 23:25:57 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from './Modal';
 export { default as DropDown } from './DropDown';
 export { default as FilterButton } from './Buttons/Filter';
 export { default as Header } from './Header';
+export { default as InfoSearch } from './InfoSearch';
 export { default as Layout } from './Layout';
 export { default as MainPage } from './MainPage';
 export { default as MentorCard } from './MentorCard';
