0f31ff21521f744c10e3c21bbed38f09028b921f
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-23 09:34:44 +0900

    Update mentorInfoFormSchema

--- src/schemas/mentorInfoForm.ts
+++ src/schemas/mentorInfoForm.ts
@@ -5,14 +5,19 @@ export const mentorInfoFormSchema = z.object({
     .string()
     .min(2, { message: '2글자 이상 입력해주세요.' })
     .max(4, { message: '4글자 이하로 입력해주세요.' }),
-  phone: z
+  phoneNumber: z
     .string()
     .length(11, { message: '전화번호는 11글자로 입력해주세요.' }),
-  SNS: z.string().url({ message: 'URL 형식으로 입력해주세요.' }).optional(),
+  email: z
+    .string()
+    .email({ message: '이메일 형식으로 입력해주세요.' })
+    .nullable(),
   generation: z
     .string({
       required_error: '기수를 입력해주세요.',
       // invalid_type_error: '기수를 입력해주세요.',
     })
-    .regex(/^[1-5]+$/, { message: '기수를 입력해주세요.' }),
+    .max(1, { message: '1글자 이하로 입력해주세요.' })
+    .regex(/^[1-5]+$/, { message: '1~5까지 입력 가능하십니다.' }),
+  snsUrl: z.string().url({ message: 'URL 형식으로 입력해주세요.' }).nullable(),
 });
