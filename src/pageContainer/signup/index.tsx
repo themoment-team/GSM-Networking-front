dfc4be42d613834af0d662de5b1bd1b523572271
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-24 16:08:01 +0900

    Add CareerRegistrationBox in signup page

--- src/pageContainer/signup/index.tsx
+++ src/pageContainer/signup/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { ChangeEvent } from 'react';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
@@ -10,11 +10,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 import * as S from './style';
 
-import { PlusIcon } from '@/assets';
-import { Header, Input, Select } from '@/components';
-import { POSITION_ARRAY } from '@/constants';
+import { CareerRegistrationBox, Header, Input } from '@/components';
+import { defaultCareer } from '@/constants';
 import { mentorInfoFormSchema } from '@/schemas';
-import type { MentorInfoFormType } from '@/types';
+import type { CareerFormType, MentorInfoFormType } from '@/types';
 
 const onlyAllowNumberInput = (e: ChangeEvent<HTMLInputElement>) => {
   const inputValue = e.target.value;
@@ -23,7 +22,9 @@ const onlyAllowNumberInput = (e: ChangeEvent<HTMLInputElement>) => {
 };
 
 const SignUp = () => {
-  const positionSelectRef = useRef<HTMLSelectElement>(null);
+  const [careerArray, setCareerArray] = useState<CareerFormType[]>([
+    defaultCareer,
+  ]);
 
   const {
     register,
@@ -77,34 +78,14 @@ const SignUp = () => {
             />
           </S.InputWrapper>
         </S.PrivacyBox>
-        <S.CompanyBox>
-          <S.TitleBox>
-            <S.Title>재직 회사 정보</S.Title>
-            <S.AddButton type='button'>
-              <PlusIcon />
-            </S.AddButton>
-          </S.TitleBox>
-          <S.InputWrapper>
-            <Input
-              {...register('name')}
-              inputTitle='회사명'
-              errorMessage={errors.name?.message}
-              required={true}
-            />
-            <Select
-              title='포지션(직책, 직무)선택'
-              defaultValue='포지션 선택'
-              options={[...POSITION_ARRAY]}
-              ref={positionSelectRef}
-            />
-            <S.PeriodTitle>재직 기간</S.PeriodTitle>
-            <S.TenureCheck type='checkbox' id='checkbox' />
-            <S.TenureCheckLabel htmlFor='checkbox'>
-              <S.CheckBox />
-              재직 중
-            </S.TenureCheckLabel>
-          </S.InputWrapper>
-        </S.CompanyBox>
+        {careerArray.map((career, index) => (
+          <CareerRegistrationBox
+            career={career}
+            setCareerArray={setCareerArray}
+            index={index}
+            key={index}
+          />
+        ))}
         <S.SubmitButton type='submit'>등록</S.SubmitButton>
       </S.Form>
     </>
