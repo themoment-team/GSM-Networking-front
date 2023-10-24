0f5ccb16e355e6b54d686f4441fdfd824db95b60
gaoooon <gaon9749@gmail.com>
2023-10-24 21:05:40 +0900

    Add style.ts

--- src/components/InfoSearch/style.ts
+++ src/components/InfoSearch/style.ts
@@ -37,7 +37,7 @@ export const SearchInput = styled.input`
   width: 100%;
   border-radius: 0.625rem;
   padding: 0.75rem 1rem;
-  background: #fff;
+  background-color: ${({ theme }) => theme.color.white};
   &::placeholder {
     color: ${({ theme }) => theme.color.grey[400]};
   }
