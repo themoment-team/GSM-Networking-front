c2cdaf1c0b4f6aec0130e9d0ec6fd95411664fd1
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-24 16:03:16 +0900

    Add constants

---
+++ src/constants/defaultCareer.ts
@@ -0,0 +1,14 @@
+import type { CareerFormType } from '@/types';
+
+const defaultCareer: CareerFormType = {
+  companyName: '',
+  companyUrl: '',
+  position: '포지션 선택',
+  startYear: '년',
+  startMonth: '월',
+  endYear: '년',
+  endMonth: '월',
+  isWorking: false,
+};
+
+export default defaultCareer;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,4 @@
+export { default as MONTH_ARRAY } from './month';
 export { default as POSITION_ARRAY } from './position';
+export { default as YEAR_ARRAY } from './year';
+export { default as defaultCareer } from './defaultCareer';
