d9e2cd9e13dc2575656df8930ef9c6bc27ea94dc
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-25 11:14:33 +0900

    Add axiosInstance

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
       ssr: true,
     },
   },
+  rewrites: async () => [
+    {
+      source: '/api/v1/:path*',
+      destination: `${process.env.API_BASE_URL}/api/v1/:path*`,
+    },
+  ],
 };
 
 module.exports = nextConfig;
