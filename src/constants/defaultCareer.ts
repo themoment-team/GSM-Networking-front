dfb2e4246a6b96f1669bda53e1e8012b2aa1aa48
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-27 08:26:51 +0900

    Update defaultCareer

--- src/constants/defaultCareer.ts
+++ src/constants/defaultCareer.ts
@@ -1,14 +1,14 @@
 import type { CareerFormType } from '@/types';
 
 const defaultCareer: CareerFormType = {
-  companyName: '',
-  companyUrl: '',
-  position: '포지션 선택',
-  startYear: '년',
-  startMonth: '월',
-  endYear: '년',
-  endMonth: '월',
-  isWorking: false,
+  companyName: { value: '', errorMessage: null },
+  companyUrl: { value: '', errorMessage: null },
+  position: { value: '포지션 선택', errorMessage: null },
+  startYear: { value: '년', errorMessage: null },
+  startMonth: { value: '월', errorMessage: null },
+  endYear: { value: '년', errorMessage: null },
+  endMonth: { value: '월', errorMessage: null },
+  isWorking: { value: false, errorMessage: null },
 };
 
 export default defaultCareer;
