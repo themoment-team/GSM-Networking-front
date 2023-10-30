58dd6c50123bfeff87b15dc9a0d8c199b2e7f25d
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-30 22:28:30 +0900

    Add SelectFormItem

---
+++ src/components/SelectFormItem/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { forwardRef } from 'react';
+
+import { FormItemWrapper, Select } from '@/components';
+import { useForwardRef } from '@/hooks';
+
+interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  selectTitle: string;
+  errorMessage?: string | null;
+  options: any[];
+  required?: boolean;
+}
+
+const SelectFormItem = forwardRef<HTMLSelectElement, Props>(
+  ({ selectTitle, errorMessage, options, required, ...attributes }, ref) => {
+    const selectRef = useForwardRef<HTMLSelectElement>(ref);
+
+    return (
+      <FormItemWrapper
+        title={selectTitle}
+        errorMessage={errorMessage}
+        required={required}
+      >
+        <Select
+          options={[...options]}
+          errorMessage={errorMessage}
+          ref={selectRef}
+          {...attributes}
+        />
+      </FormItemWrapper>
+    );
+  }
+);
+
+SelectFormItem.displayName = 'Input';
+
+export default SelectFormItem;
