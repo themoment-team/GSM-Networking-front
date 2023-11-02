5cd1db2c17e7761cc17acdf2d9cc4c2cf780cb4b
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-02 09:09:40 +0900

    Update rewrites in next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   rewrites: async () => [
     {
       source: '/api/v1/:path*',
-      destination: `${process.env.API_BASE_URL}/api/v1/:path*`,
+      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/:path*`,
     },
   ],
 };
