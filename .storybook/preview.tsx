9cd7952160d88437180f0dcc49d40eb7941acd3e
Lee-Seungje <moondgod@gmail.com>
2023-11-03 21:29:07 +0900

    Update storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -40,9 +40,9 @@ const preview: Preview = {
           },
         ],
       },
-      nextjs: {
-        appDirectory: true,
-      },
+    },
+    nextjs: {
+      appDirectory: true,
     },
     layout: 'fullscreen',
     viewport: {
