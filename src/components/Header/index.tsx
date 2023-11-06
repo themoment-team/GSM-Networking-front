8badc67b8771393f0b77ce9fca09b15bcc24253d
gaoooon <gaon9749@gmail.com>
2023-11-06 22:20:11 +0900

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,6 @@
 
 import React from 'react';
 
-import { useRouter } from 'next/navigation';
-
 import * as S from './style';
 
 import * as I from '@/assets';
@@ -12,31 +10,23 @@ interface Props {
   clearList?: () => void;
 }
 
-const Header: React.FC<Props> = ({ clearList }) => {
-  const { push } = useRouter();
-
-  const mentorRegisteClick = () => push('/register/mentor');
-
-  return (
-    <S.Header>
-      <S.Inner>
-        <S.Logo type='button' onClick={clearList}>
-          <I.GsmNetworkingIcon />
-          GSM Networking
-        </S.Logo>
-        <S.GapBox />
-        <S.RedirectBox>
-          <S.RedirectButton>멘토 컨택</S.RedirectButton>
-          <S.RedirectButton onClick={mentorRegisteClick}>
-            멘토 등록
-          </S.RedirectButton>
-        </S.RedirectBox>
-        <S.ProfileBox>
-          <I.PersonImg4 />
-        </S.ProfileBox>
-      </S.Inner>
-    </S.Header>
-  );
-};
+const Header: React.FC<Props> = ({ clearList }) => (
+  <S.Header>
+    <S.Inner>
+      <S.Logo type='button' onClick={clearList}>
+        <I.GsmNetworkingIcon />
+        GSM Networking
+      </S.Logo>
+      <S.GapBox />
+      <S.RedirectBox>
+        <S.RedirectButton href='/'>멘토 컨택</S.RedirectButton>
+        <S.RedirectButton href='/register/mentor'>멘토 등록</S.RedirectButton>
+      </S.RedirectBox>
+      <S.ProfileBox>
+        <I.PersonImg4 />
+      </S.ProfileBox>
+    </S.Inner>
+  </S.Header>
+);
 
 export default Header;
