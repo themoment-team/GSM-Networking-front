6e4f08fbaaeec2ff8f69ea271f8a8ccde6e7c022
Lee-Seungje <moondgod@gmail.com>
2023-11-07 12:28:32 +0900

    Add triangleIcon

---
+++ src/assets/TriangleIcon.tsx
@@ -0,0 +1,13 @@
+const TriangleIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='10'
+    height='6'
+    viewBox='0 0 10 6'
+    fill='none'
+  >
+    <path d='M5 0L10 6H0L5 0Z' fill='#148EFF' />
+  </svg>
+);
+
+export default TriangleIcon;
