f798aeab9e1f749cde79a685bc6d3e21abc22e9f
Lee-Seungje <moondgod@gmail.com>
2023-11-07 16:42:28 +0900

    Update unit

--- src/assets/TriangleIcon.tsx
+++ src/assets/TriangleIcon.tsx
@@ -1,9 +1,9 @@
 const TriangleIcon = () => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='10'
-    height='6'
-    viewBox='0 0 10 6'
+    width='0.625rem'
+    height='0.375rem'
+    viewBox='0 0 0.625rem 0.375rem'
     fill='none'
   >
     <path d='M5 0L10 6H0L5 0Z' fill='#148EFF' />
