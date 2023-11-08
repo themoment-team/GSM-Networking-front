6cc45e6e1204cd07af8fc5e5fa0e99730644957d
gaoooon <gaon9749@gmail.com>
2023-11-08 15:55:45 +0900

    Update index.tsx

--- src/components/InfoSearch/index.tsx
+++ src/components/InfoSearch/index.tsx
@@ -1,14 +1,30 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
 
 import * as S from './style';
 
+import { TempMentorCard } from '@/components';
+import { useGetTempMentor } from '@/hooks/api/mentor/useGetTempMentor';
+// import type { WorkerType } from '@/types';
+
 const InfoSearch: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { data } = useGetTempMentor(inputValue);
+  const [selectUser, setSelectUser] = useState<string | null>(null);
+  const isUserSelect = !!selectUser;
+  const { push } = useRouter();
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setInputValue(e.currentTarget.value);
+  // console.log(data);
+
+  const handleSearchClick = () =>
+    inputRef.current && setInputValue(inputRef.current.value);
+
+  const handleButtonClick = () =>
+    isUserSelect && push(`/register/mentor?id=${selectUser}`);
 
   return (
     <S.Container>
@@ -17,14 +33,22 @@ const InfoSearch: React.FC = () => {
         <S.SkipLink>건너뛰기</S.SkipLink>
       </S.TitleBox>
       <S.SearchContainer>
-        <S.SearchInput
-          placeholder='이름을 입력해주세요.'
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <S.SearchButton>검색</S.SearchButton>
+        <S.SearchInput placeholder='이름을 입력해주세요.' ref={inputRef} />
+        <S.SearchButton onClick={handleSearchClick}>검색</S.SearchButton>
       </S.SearchContainer>
-      <S.SkipButton>다음</S.SkipButton>
+      <S.TempMentorCardBox>
+        {data?.map((user) => (
+          <TempMentorCard
+            key={user.id}
+            worker={user}
+            selectUser={selectUser}
+            setSelectUser={setSelectUser}
+          />
+        ))}
+      </S.TempMentorCardBox>
+      <S.Button onClick={handleButtonClick} isSelect={isUserSelect}>
+        다음
+      </S.Button>
     </S.Container>
   );
 };
