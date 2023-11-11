be8a23c3d841cf1dec914691f078f6fd571c559d
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-12 00:05:02 +0900

    Add UTCDate

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as UTCDate } from './UTCDate';
 export { default as extractSubstring } from './extractSubstring';
 export { default as isNumeric } from './isNumeric';
 export { default as minutesToSeconds } from './minutesToSeconds';
