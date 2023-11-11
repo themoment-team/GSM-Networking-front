c25ea2b66f1c26b1bdabebbfde4aa791bacfe1c0
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-12 00:29:07 +0900

    Add deepCopy util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as deepCopy } from './deepCopy';
 export { default as extractSubstring } from './extractSubstring';
 export { default as isNumeric } from './isNumeric';
 export { default as minutesToSeconds } from './minutesToSeconds';
