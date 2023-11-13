9438273ae8f07ecbf878932005f8d1ab0ecfee2f
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-13 16:13:54 +0900

    Add metadate at MentorRegisterPage

--- src/app/register/mentor/page.tsx
+++ src/app/register/mentor/page.tsx
@@ -1,5 +1,12 @@
 import { MentorRegister } from '@/pageContainer';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '멘토 등록',
+  description: '멘토 등록 페이지입니다.',
+};
+
 export default function MentorRegisterPage() {
   return <MentorRegister />;
 }
