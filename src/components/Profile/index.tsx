a3a5141f8490b8d30cd2fe8e1032341246e8f823
Lee-Seungje <moondgod@gmail.com>
2023-11-16 07:52:15 +0900

    Add annotate

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -10,6 +10,7 @@ interface Props {
 }
 
 const Profile: React.FC<Props> = ({ profile: { name, generation } }) => {
+  // TODO : server side 와 마크업을 일치시키기 위한 로직 변경 필요.
   const randomValue = Math.floor(Math.random() * 5); //0부터 4까지 중의 랜덤값 생성
 
   const handleClick = () => {};
