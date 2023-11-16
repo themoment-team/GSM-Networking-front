96dd41d8198dd774e1039503c175fcd15260fc94
Lee-Seungje <moondgod@gmail.com>
2023-11-16 16:14:10 +0900

    Add searchMentor story

--- src/components/CannotFind/index.stories.tsx
+++ src/components/CannotFind/index.stories.tsx
@@ -13,7 +13,16 @@ export default meta;
 
 type Story = StoryObj<typeof CannotFind>;
 
-export const Primary: Story = {
+export const SearchMentor: Story = {
+  args: {
+    textArr: [
+      '해당 정보를 가진 졸업생을 찾을 수 없어요.',
+      '오타, 띄어쓰기 등을 확인 해주세요.',
+    ],
+  },
+};
+
+export const ResistorSearchMentor: Story = {
   args: {
     textArr: ['이름을 찾을 수 없어요.', '오타, 띄어쓰기 등을 확인 해주세요.'],
   },
