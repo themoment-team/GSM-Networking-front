2e57dc9bd1e4e1e29650ae2987d97ef077f8fedc
gaoooon <gaon9749@gmail.com>
2023-11-16 20:04:12 +0900

    Add index.tsx

---
+++ src/components/CareerCard/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+
+import * as S from './style';
+
+import type { CareerType } from '@/types';
+
+interface Props {
+  career: CareerType;
+}
+
+const index: React.FC<Props> = ({ career }) => {
+  const careerStartYear = career.startDate.getFullYear();
+  const tempStartMonth = career.startDate.getMonth() + 1;
+  const careerStartMonth =
+    tempStartMonth < 10 ? `0${tempStartMonth}` : tempStartMonth;
+
+  const careerEndYear = career.endDate?.getFullYear();
+  const tempEndMonth = career.endDate?.getMonth() ?? 0 + 1;
+  const careerEndMonth =
+    tempEndMonth < 10 ? `0${tempStartMonth}` : tempStartMonth;
+
+  return (
+    <S.CareerCardContainer>
+      <S.CareerInfoBox>
+        <S.CompanyName>{career.companyName}</S.CompanyName>
+        <S.position>{career.position} 🖥️</S.position>
+        <S.CareerPeriod isWorking={career.isWorking}>
+          {career.isWorking
+            ? '재직중'
+            : `${careerStartYear}.${careerStartMonth} ~ ${careerEndYear}.${careerEndMonth}`}
+        </S.CareerPeriod>
+      </S.CareerInfoBox>
+    </S.CareerCardContainer>
+  );
+};
+export default index;
