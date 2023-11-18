467b3dd2a15b9db03692f0dbe317c440ceda65b1
Lee-Seungje <moondgod@gmail.com>
2023-11-18 12:44:35 +0900

    Add myPage

---
+++ src/pageContainer/mypage/index.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import * as S from './style';
+
+import { Profile, CareerCard } from '@/components';
+import type { CareerType } from '@/types';
+
+interface MentorInfoType {
+  id: number;
+  name: string;
+  email: string;
+  generation: number;
+  SNS?: string;
+  profileUrl?: string;
+  registerd: boolean;
+  career: CareerType[];
+}
+
+interface Props {
+  mentorInfo: MentorInfoType;
+}
+
+const MyPage: React.FC<Props> = ({
+  mentorInfo: { name, generation, career: careerList },
+}) => {
+  const a = 0;
+
+  return (
+    <div>
+      <S.ProfileContainer>
+        <Profile name={name} generation={generation} />
+      </S.ProfileContainer>
+      <S.Line />
+      <S.CareerContainer>
+        <S.CareerInfoText>재직 정보</S.CareerInfoText>
+        <S.CareerBox>
+          {careerList.map((career, i) => (
+            <CareerCard career={career} key={i /*career.id 수정 */} />
+          ))}
+        </S.CareerBox>
+      </S.CareerContainer>
+    </div>
+  );
+};
+
+export default MyPage;
---
+++ src/pageContainer/mypage/style.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 56px 0;
+`;
+
+export const Line = styled.div`
+  width: 601px;
+  height: 12px;
+  border-radius: 1px;
+  background: ${({ theme }) => theme.color.grey[50]};
+`;
+
+export const CareerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const CareerInfoText = styled.span`
+  color: ${({ theme }) => theme.color.grey[900]};
+
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 150%;
+`;
+
+export const CareerBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px 16px;
+`;
