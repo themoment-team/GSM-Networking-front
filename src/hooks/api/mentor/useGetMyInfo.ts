ffd3e61b3eb056ab927680cc505e2ba8b9d7663f
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-19 15:44:05 +0900

    Add useGetMyInfo

---
+++ src/hooks/api/mentor/useGetMyInfo.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { get, mentorQueryKeys, mentorUrl } from '@/libs';
+import type { MentorInfoType } from '@/types';
+import { minutesToMs } from '@/utils';
+
+import type { UseQueryOptions } from '@tanstack/react-query';
+
+export const useGetMyInfo = (options?: Omit<UseQueryOptions, 'queryKey'>) =>
+  useQuery({
+    queryKey: mentorQueryKeys.getMyInfo(),
+    queryFn: () => get<MentorInfoType>(mentorUrl.getMyInfo()),
+    staleTime: minutesToMs(5),
+    gcTime: minutesToMs(5),
+    ...options,
+  });
--- src/libs/api/requestUrlController.ts
+++ src/libs/api/requestUrlController.ts
@@ -16,6 +16,7 @@ export const mentorUrl = {
   postMentorRegister: () => '/mentor',
   getMentorList: () => '/mentor',
   deleteMyMentorData: () => '/mentor/my',
+  getMyInfo: () => '/mentor/my',
 };
 
 export const tempMentorUrl = {
