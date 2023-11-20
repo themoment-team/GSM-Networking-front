740fab310d3c7f313e3d9af298d772a9e1bb4ab4
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-20 19:22:59 +0900

    Add MyPageModalWrapper

---
+++ src/components/Modal/MyPage/ModalWrapper/index.tsx
@@ -0,0 +1,34 @@
+/**@jsxImportSource @emotion/react */
+
+import React from 'react';
+
+import type { Interpolation, Theme } from '@emotion/react';
+
+import * as S from './style';
+
+interface Props {
+  children: React.ReactNode;
+  closeModal: () => void;
+  innerCss?: Interpolation<Theme>;
+}
+
+const MyPageModalWrapper: React.FC<Props> = ({
+  children,
+  closeModal,
+  innerCss,
+}) => {
+  const handleModalWrapperClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    closeModal();
+    e.stopPropagation();
+  };
+
+  return (
+    <S.ModalWrapper onClick={handleModalWrapperClick}>
+      <S.ModalInner css={innerCss}>{children}</S.ModalInner>
+    </S.ModalWrapper>
+  );
+};
+
+export default MyPageModalWrapper;
