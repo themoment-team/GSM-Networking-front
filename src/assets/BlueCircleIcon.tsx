e313dfa1de6b6198f10e4e915da085e38d1ab67f
gaoooon <gaon9749@gmail.com>
2023-11-21 23:48:47 +0900

    Add BlueCircleIcon.tsx

---
+++ src/assets/BlueCircleIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BlueCircleIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='8'
+    height='8'
+    viewBox='0 0 8 8'
+    fill='none'
+  >
+    <circle
+      cx='4'
+      cy='4'
+      r='4'
+      transform='matrix(-1 0 0 1 8 0)'
+      fill='#148EFF'
+    />
+  </svg>
+);
+
+export default BlueCircleIcon;
