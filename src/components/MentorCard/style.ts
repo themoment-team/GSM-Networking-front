8a8ed3eab9bda757e3fbc4d326837bf4f859c5f6
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-23 14:55:13 +0900

    Update WorkerImgBox height

--- src/components/MentorCard/style.ts
+++ src/components/MentorCard/style.ts
@@ -93,7 +93,7 @@ export const WorkerImgBox = styled.div`
   justify-content: center;
   align-items: end;
   width: 100%;
-  height: 6.8125rem;
+  height: 10rem;
   position: relative;
   overflow: hidden;
   border: 0.0625rem solid ${({ theme }) => theme.color.grey[50]};
