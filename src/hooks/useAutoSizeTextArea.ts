873d4749fcc8d916437bcb1dcf9c62b2354ab2e3
Lee-Seungje <moondgod@gmail.com>
2023-11-23 19:29:06 +0900

    Add useAutoSizeTextArea

---
+++ src/hooks/useAutoSizeTextArea.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+export const useAutosizeTextArea = (
+  textAreaRef: HTMLTextAreaElement | null,
+  value: string
+) => {
+  useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = '0px';
+      const scrollHeight = textAreaRef.scrollHeight;
+      textAreaRef.style.height = scrollHeight + 'px';
+    }
+  }, [textAreaRef, value]);
+};
