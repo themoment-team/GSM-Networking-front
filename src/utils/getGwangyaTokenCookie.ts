ac8d0b3f9ef860e8396a8bc6fb64d61a295b393e
hyeongrok7874 <khr5k0228@gmail.com>
2023-11-27 20:17:41 +0900

    Add getGwangyaTokenCookie

---
+++ src/utils/getGwangyaTokenCookie.ts
@@ -0,0 +1,9 @@
+const getGwangyaTokenCookie = () => {
+  if (typeof document === 'undefined') return '';
+
+  const cookies = document.cookie;
+  const gwangyaToken = cookies.split('gwangyaToken=')[1]?.slice(0, 20) ?? '';
+  return gwangyaToken;
+};
+
+export default getGwangyaTokenCookie;
