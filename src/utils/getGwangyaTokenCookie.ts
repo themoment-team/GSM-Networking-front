a079b7f2d4e51c2279fabcd5cd55f75f8cc7a9fa
Lee-Seungje <moondgod@gmail.com>
2023-11-27 23:48:35 +0900

    Resolve build error

--- src/utils/getGwangyaTokenCookie.ts
+++ src/utils/getGwangyaTokenCookie.ts
@@ -1,9 +1,9 @@
 const getGwangyaTokenCookie = () => {
+  if (typeof document === 'undefined') return '';
   const cookies = document.cookie;
-  if (cookies.includes('gwangyaToken')) {
-    const gwangyaToken = cookies.split('gwangyaToken=')[1].slice(0, 20);
-    return gwangyaToken;
-  }
+
+  const gwangyaToken = cookies.split('gwangyaToken=')[1].slice(0, 20);
+  return gwangyaToken;
 };
 
 export default getGwangyaTokenCookie;
