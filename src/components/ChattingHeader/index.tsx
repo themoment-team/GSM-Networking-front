6c11c47d6eef18c3391cd0e07f133b5f0b430eb3
gaoooon <gaon9749@gmail.com>
2023-12-06 14:56:10 +0900

    Add index.tsx

---
+++ src/components/ChattingHeader/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import * as S from './style';
+
+import { GoBackIcon } from '@/assets';
+
+interface Props {
+  name: string;
+  generation: number;
+}
+
+const ChattingHeader: React.FC<Props> = ({ name, generation }) => {
+  const { push } = useRouter();
+
+  // 어디로 이동해야 할지 정해지지 않아 /로 이동하게 해두었습니다!
+  const handleGoBackIconClick = () => push('/');
+
+  return (
+    <S.Container>
+      <S.IconBox onClick={handleGoBackIconClick}>
+        <GoBackIcon />
+      </S.IconBox>
+      <S.OpponentInfo>
+        <S.Generation>{generation}기 </S.Generation>
+        <S.Name>{name}</S.Name>
+      </S.OpponentInfo>
+      <S.SpaceBox />
+    </S.Container>
+  );
+};
+
+export default ChattingHeader;
