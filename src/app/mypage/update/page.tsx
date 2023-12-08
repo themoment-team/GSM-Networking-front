8fed316c37e36ea9e382c9cf8b3289061d3f7a72
Lee-Seungje <moondgod@gmail.com>
2023-12-08 14:21:47 +0900

    Add myInfoUpdatePage

---
+++ src/app/mypage/update/page.tsx
@@ -0,0 +1,11 @@
+import { MyInfoUpdate } from '@/pageContainer';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '내 정보 수정',
+};
+
+const MyInfoUpdatePage = () => <MyInfoUpdate />;
+
+export default MyInfoUpdatePage;
--- src/pageContainer/index.ts
+++ src/pageContainer/index.ts
@@ -1,5 +1,6 @@
 export { default as Gwangya } from './gwangya';
 export { default as MentorRegister } from './register/mentor';
 export { default as MyInfoPage } from './mypage';
+export { default as MyInfoUpdate } from './mypage/update';
 export { default as SignIn } from './signin';
 export { default as SignUp } from './signup';
