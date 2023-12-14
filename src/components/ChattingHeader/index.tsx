8d48edfef80fdad7855e464ff1606b5d3ea49eb2
gaoooon <gaon9749@gmail.com>
2023-12-14 17:47:44 +0900

    Update function name

--- src/components/ChattingHeader/index.tsx
+++ src/components/ChattingHeader/index.tsx
@@ -15,11 +15,11 @@ const ChattingHeader: React.FC<Props> = ({ name, generation }) => {
   const { push } = useRouter();
 
   // 어디로 이동해야 할지 정해지지 않아 /로 이동하게 해두었습니다!
-  const handleGoBackIconClick = () => push('/');
+  const handleGoBackClick = () => push('/');
 
   return (
     <S.Container>
-      <S.IconBox onClick={handleGoBackIconClick}>
+      <S.IconBox onClick={handleGoBackClick}>
         <GoBackIcon />
       </S.IconBox>
       <S.OpponentInfo>
