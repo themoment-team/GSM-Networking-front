608a13c8d893cdfe3e1d92b0d680a27f62a5c450
gaoooon <gaon9749@gmail.com>
2023-12-27 23:22:49 +0900

    Update index.tsx

--- src/pageContainer/register/mentor/index.tsx
+++ src/pageContainer/register/mentor/index.tsx
@@ -18,7 +18,7 @@ import {
   InputFormItem,
   SelectFormItem,
 } from '@/components';
-import { GENERATION_ARRAY } from '@/constants';
+import { MENTOR_GENERATION_ARRAY } from '@/constants';
 import {
   useDeleteTempMentor,
   useGetMyInfo,
@@ -219,7 +219,7 @@ const MentorRegister: React.FC<Props> = ({ tempMentorId, mentorInfo }) => {
             <SelectFormItem
               {...register('generation')}
               selectTitle='기수'
-              options={[...GENERATION_ARRAY]}
+              options={[...MENTOR_GENERATION_ARRAY]}
               defaultValue='기수를 선택해주세요.'
               errorMessage={errors.generation?.message}
               required={true}
