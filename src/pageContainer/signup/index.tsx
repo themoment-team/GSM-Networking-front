ab711f6b25f42e61df03ae5aacf56ff798b19caf
gaoooon <gaon9749@gmail.com>
2023-12-30 17:59:00 +0900

    Update index.tsx

--- src/pageContainer/signup/index.tsx
+++ src/pageContainer/signup/index.tsx
@@ -11,6 +11,7 @@ import * as S from './style';
 import { CheckBoxIcon } from '@/assets';
 import { UserStatusSelect } from '@/components';
 import { PRIVACY_POLICY_URL } from '@/constants';
+import { usePostMenteeAuthority } from '@/hooks';
 
 const SignUp = () => {
   const [role, setRole] = useState<'mentee' | 'mentor' | null>(null);
@@ -18,21 +19,21 @@ const SignUp = () => {
 
   const { push } = useRouter();
 
-  // const { mutate: menteeRoleMutate } = usePostMenteeRole({
-  //   onError: () => toast.error('멘티 등록에 실패했습니다.'),
-  //   onSuccess: () => handleMutateSuccess(),
-  // });
+  const { mutate: mutateMenteeAuthority } = usePostMenteeAuthority({
+    onError: () => toast.error('멘티 등록에 실패했습니다.'),
+    onSuccess: () => handleMutateSuccess(),
+  });
 
-  // const handleMutateSuccess = () => {
-  //   toast.success('멘티 등록에 성공했습니다.');
-  //   push('/');
-  // };
+  const handleMutateSuccess = () => {
+    toast.success('멘티 등록에 성공했습니다.');
+    push('/register/mentee');
+  };
 
   const handleNextButtonClick = () => {
     if (checkPolicy === false)
       return toast.error('개인정보 처리방침에 동의해주세요.');
 
-    if (role === 'mentee') return push('/register/mentee');
+    if (role === 'mentee') mutateMenteeAuthority();
 
     if (role === 'mentor') return push('/register/search');
   };
