2d08486e357b53594c09f420c09e6321a4a894a1
Lee-Seungje <moondgod@gmail.com>
2024-01-15 18:11:14 +0900

    Delete request type

--- src/hooks/api/mentor/usePutMentorUpdate.ts
+++ src/hooks/api/mentor/usePutMentorUpdate.ts
@@ -1,26 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { mentorQueryKeys, mentorUrl, put } from '@/libs';
-import type { MentorType, RequestCareerType } from '@/types';
+import type { MentorType } from '@/types';
 
 import type { UseMutationOptions } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 
-interface CareerType extends RequestCareerType {
-  id?: number;
-}
-
-interface RequestType extends MentorType {
-  id?: number;
-  career: CareerType[];
-}
-
 export const usePutMentorUpdate = (
-  options?: UseMutationOptions<unknown, AxiosError, RequestType, unknown>
+  options?: UseMutationOptions<unknown, AxiosError, MentorType, unknown>
 ) =>
   useMutation({
     mutationKey: mentorQueryKeys.putMentorUpdate(),
-    mutationFn: (mentorInfo: RequestType) =>
+    mutationFn: (mentorInfo: MentorType) =>
       put(mentorUrl.putMentorUpdate(), mentorInfo),
     ...options,
   });
