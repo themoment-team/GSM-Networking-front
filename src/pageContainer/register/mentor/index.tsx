93dfdf4a4ee9eb802e18c06119d73570b38ea83a
이승제 <106712562+frorong@users.noreply.github.com>
2024-01-15 18:13:15 +0900

    Update src/pageContainer/register/mentor/index.tsx
    Co-authored-by: 김형록 <khr5k0228@gmail.com>

--- src/pageContainer/register/mentor/index.tsx
+++ src/pageContainer/register/mentor/index.tsx
@@ -79,9 +79,6 @@ const MentorRegister: React.FC<Props> = ({ tempMentorId, mentorInfo }) => {
 
   const handleMentorUpdateSuccess = () => {
     toast.success('멘토 수정에 성공하였습니다.');
-    if (tempMentorId) {
-      return mutateDeleteTempMentor(tempMentorId);
-    }
     return push('/mypage');
   };
 
