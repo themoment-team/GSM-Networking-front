89e14e94df31bf365d3a2af953cd73783de6fb4a
gaoooon <gaon9749@gmail.com>
2024-03-12 23:19:10 +0900

    Add prettier

--- src/components/ChattingDate/index.tsx
+++ src/components/ChattingDate/index.tsx
@@ -1,26 +1,26 @@
-'use client'
+'use client';
 
 import * as S from './style';
 
 interface ChattingDateType {
-    createdAt: string;
+  createdAt: string;
 }
 
 const ChattingDate: React.FC<ChattingDateType> = ({ createdAt }) => {
-    const createdDate = new Date(createdAt + 'Z');
-    let createdTime = createdDate.getHours();
-    const createdMinute = createdDate.getMinutes();
-    const isMorning = createdTime < 12;
-    createdTime = createdTime % 12 || 12;
+  const createdDate = new Date(createdAt + 'Z');
+  let createdTime = createdDate.getHours();
+  const createdMinute = createdDate.getMinutes();
+  const isMorning = createdTime < 12;
+  createdTime = createdTime % 12 || 12;
 
-    const formattedTime = createdTime.toString().padStart(2, '0');
-    const formattedMinute = createdMinute.toString().padStart(2, '0');
+  const formattedTime = createdTime.toString().padStart(2, '0');
+  const formattedMinute = createdMinute.toString().padStart(2, '0');
 
-    return (
-        <S.DateText>
-            {isMorning ? '오전' : '오후'} {formattedTime}:{formattedMinute}
-        </S.DateText>
-    )
-}
+  return (
+    <S.DateText>
+      {isMorning ? '오전' : '오후'} {formattedTime}:{formattedMinute}
+    </S.DateText>
+  );
+};
 
 export default ChattingDate;
