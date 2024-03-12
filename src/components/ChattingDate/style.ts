d46541afd533cf3a1ac6cc5f74d0a1db9548fdd6
gjaegyun <jaegyungim06@gmail.com>
2024-03-12 17:49:36 +0900

    Change span

--- src/components/ChattingDate/style.ts
+++ src/components/ChattingDate/style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const DateText = styled.p`
+export const DateText = styled.span`
     ${({ theme }) => theme.typo.caption};
     color: ${({ theme }) => theme.color.grey[500]};
 `;
