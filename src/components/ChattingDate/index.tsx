7a0edce3cee61336b80ecca2270fec2e4101f424
gjaegyun <jaegyungim06@gmail.com>
2024-03-13 16:06:55 +0900

    Change location

--- src/components/ChattingDate/index.tsx
+++ src/components/ChattingDate/index.tsx
@@ -6,17 +6,17 @@ interface ChattingDateType {
   createdAt: string;
 }
 
+const Day = {
+  morning: '오전',
+  afternoon: '오후',
+} as const
+
+const HOURS_IN_HALF_DAY = 12 as const;
+
 const ChattingDate: React.FC<ChattingDateType> = ({ createdAt }) => {
   const createdDate = new Date(createdAt + 'Z');
   let createdHour = createdDate.getHours();
   const createdMinute = createdDate.getMinutes();
-
-  const Day = {
-    morning: '오전',
-    afternoon: '오후',
-  } as const
-
-  const HOURS_IN_HALF_DAY = 12 as const;
 
   const period = createdHour < HOURS_IN_HALF_DAY ? Day.morning : Day.afternoon;
   createdHour = createdHour % HOURS_IN_HALF_DAY || HOURS_IN_HALF_DAY;
