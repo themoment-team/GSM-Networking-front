21ada49f73404835a2bc2820a20742d5a011dbd5
gjaegyun <jaegyungim06@gmail.com>
2024-03-13 15:56:12 +0900

    Change const&&object

--- src/components/ChattingDate/index.tsx
+++ src/components/ChattingDate/index.tsx
@@ -10,8 +10,16 @@ const ChattingDate: React.FC<ChattingDateType> = ({ createdAt }) => {
   const createdDate = new Date(createdAt + 'Z');
   let createdHour = createdDate.getHours();
   const createdMinute = createdDate.getMinutes();
-  const period = createdHour < 12 ? '오전' : '오후';
-  createdHour = createdHour % 12 || 12;
+
+  const Day = {
+    morning: '오전',
+    afternoon: '오후',
+  } as const
+
+  const HOURS_IN_HALF_DAY = 12 as const;
+
+  const period = createdHour < HOURS_IN_HALF_DAY ? Day.morning : Day.afternoon;
+  createdHour = createdHour % HOURS_IN_HALF_DAY || HOURS_IN_HALF_DAY;
 
   const formattedTime = createdHour.toString().padStart(2, '0');
   const formattedMinute = createdMinute.toString().padStart(2, '0');
