33b16688b517b75a847ad16e96ceae7c39b1a70c
gaoooon <gaon9749@gmail.com>
2024-03-18 13:15:12 +0900

    Update index.tsx

--- src/pageContainer/chat/list/index.tsx
+++ src/pageContainer/chat/list/index.tsx
@@ -8,25 +8,38 @@ import SockJS from 'sockjs-client';
 import * as S from './style';
 
 import { Header } from '@/components';
+import { getSessionId } from '@/utils';
 
-import type { Client, CompatClient } from '@stomp/stompjs';
+import type { Client } from '@stomp/stompjs';
 
 const ChatList = () => {
-  const client = useRef<Client | CompatClient | null>(null);
+  const client = useRef<Client | null>(null);
 
   useEffect(() => {
-    const sock = new SockJS('https://server.gsm-networking.com:8080/ws', {
-      withCredentials: true,
-    });
+    const sock = new SockJS('https://server.gsm-networking.com:8080/ws');
     client.current = Stomp.over(() => sock);
 
-    client.current.connectHeaders = {};
+    client.current.connectHeaders = {
+      access_token:
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMyIsInRva2VuVHlwZSI6ImFjY2Vzc1Rva2VuIiwiaWF0IjoxNzEwNTk5NjQwLCJleHAiOjE3MTA2MTA0NDB9.iqmkFjdPkTpPLE4UCfhmFfwqYQw0wXoDJUNtL7kU0UM',
+    };
     client.current.onConnect = () => {
+      console.log(getSessionId(sock));
+      client.current?.subscribe(
+        `/queue/user/${getSessionId(sock)}`,
+        (message) => {
+          console.log(message);
+        }
+      );
       // client.current.subscribe('/query/header', (message) => {
       // });
     };
 
     client.current.activate();
+
+    return () => {
+      client.current?.deactivate();
+    };
   }, []);
   return (
     <>
