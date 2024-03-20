0dd5ffb0f7aca26678574d5a26d7b1708fc665b6
Lee-Seungje <moondgod@gmail.com>
2024-03-20 19:49:55 +0900

    Update miniprofile

--- src/components/MiniProfile/index.stories.tsx
+++ src/components/MiniProfile/index.stories.tsx
@@ -15,16 +15,17 @@ type Story = StoryObj<typeof MiniProfile>;
 
 export const Primary: Story = {
   args: {
-    name: '김형록',
-    generation: 5,
+    profile: { name: '김형록', generation: 5 },
   },
 };
 
 export const HavedImage: Story = {
   args: {
-    name: '김형록',
-    generation: 5,
-    profileUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmut5h67L7lxQoTWdCnEQtWxh1LNN-sWgQw&usqp=CAU',
+    profile: {
+      name: '김형록',
+      generation: 5,
+      profileUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmut5h67L7lxQoTWdCnEQtWxh1LNN-sWgQw&usqp=CAU',
+    },
   },
 };
--- src/components/MiniProfile/index.tsx
+++ src/components/MiniProfile/index.tsx
@@ -7,12 +7,16 @@ import * as S from './style';
 import { RandomMentorImg } from '@/components';
 
 interface Props {
-  name: string;
-  generation: number;
-  profileUrl?: string;
+  profile: {
+    name: string;
+    generation: number;
+    profileUrl?: string;
+  };
 }
 
-const MiniProfile: React.FC<Props> = ({ name, generation, profileUrl }) => {
+const MiniProfile: React.FC<Props> = ({
+  profile: { name, generation, profileUrl },
+}) => {
   // TODO : server side 와 마크업을 일치시키기 위한 로직 변경 필요.
   const randomValue = Math.floor(Math.random() * 5); //0부터 4까지 중의 랜덤값 생성
 
