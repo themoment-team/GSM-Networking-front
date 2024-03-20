3af3643c7ab99fb8fe4dc4498919ce87902fd957
예빈 <yebin060310@gmail.com>
2024-03-20 20:27:56 +0900

    Delete return

--- src/components/NoticeCard/index.tsx
+++ src/components/NoticeCard/index.tsx
@@ -10,26 +10,24 @@ interface Props {
   tag: string;
 }
 
-const NoticeCard: React.FC<Props> = ({ name, date, time, content, tag }) => {
-  return (
-    <S.NoticeCard>
-      <S.ContentBox>
-        <S.ContentHeader>
-          <S.HeaderTitle>
-            <S.Name>{name}</S.Name>
-            <S.DateAndTime>
-              <S.Date>{date}</S.Date>
-              <S.Time>{time}</S.Time>
-            </S.DateAndTime>
-          </S.HeaderTitle>
-          <S.Tag>{tag}</S.Tag>
-        </S.ContentHeader>
-        <S.Content maxLength={50} readOnly>
-          {content}
-        </S.Content>
-      </S.ContentBox>
-    </S.NoticeCard>
-  );
-};
+const NoticeCard: React.FC<Props> = ({ name, date, time, content, tag }) => (
+  <S.NoticeCard>
+    <S.ContentBox>
+      <S.ContentHeader>
+        <S.HeaderTitle>
+          <S.Name>{name}</S.Name>
+          <S.DateAndTime>
+            <S.Date>{date}</S.Date>
+            <S.Time>{time}</S.Time>
+          </S.DateAndTime>
+        </S.HeaderTitle>
+        <S.Tag>{tag}</S.Tag>
+      </S.ContentHeader>
+      <S.Content maxLength={50} readOnly>
+        {content}
+      </S.Content>
+    </S.ContentBox>
+  </S.NoticeCard>
+);
 
 export default NoticeCard;
