a82886f3384e2ca0158ebea659a002c6fe5b7e9f
예빈 <yebin060310@gmail.com>
2024-03-20 20:38:55 +0900

    Delete like button

--- src/components/NoticeCard/index.tsx
+++ src/components/NoticeCard/index.tsx
@@ -7,37 +7,27 @@ interface Props {
   date: string;
   time: string;
   content: string;
-  like: string;
   tag: string;
 }
 
-const NoticeCard: React.FC<Props> = ({
-  name,
-  date,
-  time,
-  content,
-  like,
-  tag,
-}) => {
-  return (
-    <S.NoticeCard>
-      <S.ContentBox>
-        <S.ContentHeader>
-          <S.HeaderTitle>
-            <S.Name>{name}</S.Name>
-            <S.DateAndTime>
-              <S.Date>{date}</S.Date>
-              <S.Time>{time}</S.Time>
-            </S.DateAndTime>
-          </S.HeaderTitle>
-          <S.Tag>{tag}</S.Tag>
-        </S.ContentHeader>
-        <S.Content maxLength={50} readOnly>
-          {content}
-        </S.Content>
-      </S.ContentBox>
-    </S.NoticeCard>
-  );
-};
+const NoticeCard: React.FC<Props> = ({ name, date, time, content, tag }) => (
+  <S.NoticeCard>
+    <S.ContentBox>
+      <S.ContentHeader>
+        <S.HeaderTitle>
+          <S.Name>{name}</S.Name>
+          <S.DateAndTime>
+            <S.Date>{date}</S.Date>
+            <S.Time>{time}</S.Time>
+          </S.DateAndTime>
+        </S.HeaderTitle>
+        <S.Tag>{tag}</S.Tag>
+      </S.ContentHeader>
+      <S.Content maxLength={50} readOnly>
+        {content}
+      </S.Content>
+    </S.ContentBox>
+  </S.NoticeCard>
+);
 
 export default NoticeCard;
