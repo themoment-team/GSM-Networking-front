176a061d637d559d12a29f2f157c9c0c4de32387
예빈 <yebin060310@gmail.com>
2024-03-20 20:51:02 +0900

    Update unit

--- src/assets/WriteIcon.tsx
+++ src/assets/WriteIcon.tsx
@@ -1,7 +1,7 @@
 const WriteButton = () => (
   <svg
-    width='18'
-    height='18'
+    width='1.125rem'
+    height='1.125rem'
     viewBox='0 0 18 18'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
