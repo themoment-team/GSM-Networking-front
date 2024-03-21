e948e71a2972d6f9070194b48aff0035cfe183b3
예빈 <yebin060310@gmail.com>
2024-03-21 12:01:20 +0900

    Add date format

--- src/components/NoticeCard/index.stories.tsx
+++ src/components/NoticeCard/index.stories.tsx
@@ -16,8 +16,7 @@ type Story = StoryObj<typeof NoticeCard>;
 export const Primary: Story = {
   args: {
     name: '김형록',
-    date: '11월 21일',
-    time: '오전 10:10',
+    date: '2005-02-12T00:00:00.000Z',
     content:
       '커뮤니티는사람들이모여서서로의관심사를공유하고소통하는공간으로, 다양한경험과지식을교환하며서로를돕고',
     tag: '#스터디',
