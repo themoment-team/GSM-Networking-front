b77698707279eeb48356256f08522c8b44d29da7
Lee-Seungje <moondgod@gmail.com>
2024-03-21 21:06:05 +0900

    Update chattingListPage

--- src/pageContainer/chat/list/index.tsx
+++ src/pageContainer/chat/list/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { Stomp } from '@stomp/stompjs';
+import { Client as StompClient } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 import * as S from './style';
@@ -10,73 +10,110 @@ import * as S from './style';
 import { Header } from '@/components';
 import { authUrl, get } from '@/libs';
 import type { TokenType } from '@/types';
-import { getSessionId, getMyId } from '@/utils';
+import { getMyId, getSessionId } from '@/utils';
 
 import type { Client } from '@stomp/stompjs';
 
+interface Content {
+  content: string;
+  sender?: string;
+}
+
+interface SocketType extends WebSocket {
+  _transport: { url: string };
+}
+
 const ChatList = () => {
   const client = useRef<Client | null>(null);
+  const [messages, setMessages] = useState<Content[]>([]);
+
+  const getAccessToken = async () => {
+    const { accessToken } = await get<TokenType>(authUrl.getMyToken());
+    return accessToken;
+  };
+
+  const handleClient = async () => {
+    const socket = new SockJS(`${process.env.NEXT_PUBLIC_API_BASE_URL}/ws`);
+
+    const accessToken = await getAccessToken();
+
+    client.current = new StompClient({
+      connectHeaders: {
+        access_token: accessToken,
+      },
+      webSocketFactory: () => socket,
+      debug: (str) => {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+
+      onConnect: () => {
+        console.log('WebSocket 연결이 열렸습니다.');
+        const subscriptionDestination = `/queue/user/${getSessionId(
+          socket as unknown as SocketType
+        )}`;
+
+        if (client.current) {
+          client.current.subscribe(subscriptionDestination, (frame) => {
+            try {
+              const parsedMessage = JSON.parse(frame.body);
+
+              console.log(parsedMessage);
+              setMessages((prevMessages) => [...prevMessages, parsedMessage]);
+            } catch (error) {
+              console.error('오류가 발생했습니다:', error);
+            }
+          });
+
+          client.current.subscribe(`/topic/messaging/13-14`, (frame) => {
+            try {
+              const parsedMessage = JSON.parse(frame.body);
+
+              console.log(parsedMessage);
+              setMessages((prevMessages) => [...prevMessages, parsedMessage]);
+            } catch (error) {
+              console.error('오류가 발생했습니다:', error);
+            }
+          });
+        }
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
 
   useEffect(() => {
-    const connect = async () => {
-      const sock = new SockJS('https://server.gsm-networking.com:8080/ws');
-      client.current = Stomp.over(() => sock);
-      const { accessToken } = await get<TokenType>(authUrl.getMyToken());
-
-      client.current.connectHeaders = {
-        access_token: accessToken,
-      };
-      client.current.heartbeatOutgoing = 2000;
-
-      client.current.onConnect = async (frame) => {
-        const userId = await getMyId();
-
-        // console.log(userId);
-        // const encoder = new TextEncoder();
-        // const binaryData = encoder.encode(
-        //   JSON.stringify({
-        //     userId: userId,
-        //     epochMilli: Date.now(),
-        //     limit: 5,
-        //   })
-        // );
-
-        client.current?.subscribe(
-          `/queue/user/${getSessionId(sock)}`,
-          (message) => {
-            setInterval(() => console.log(message), 3000);
-          }
-        );
-
-        client.current?.publish({
-          destination: '/header/query',
-          // binaryBody: binaryData,
-          body: JSON.stringify({
-            userId: userId,
-            epochMilli: Date.now(),
-            limit: 5,
-          }),
-          headers: {
-            'content-type': 'application/json',
-          },
-        });
-      };
-
-      client.current.activate();
-    };
-
-    connect();
-
-    return () => {
-      client.current!.deactivate();
-    };
+    handleClient();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const sendMessage = async () => {
+    // 메시지 전송
+    const id = Number(await getMyId());
+    if (client.current) {
+      const publishDestination = `/message`;
+
+      client.current.publish({
+        destination: publishDestination,
+        body: JSON.stringify({
+          to: id,
+          message: '안녕하세요',
+        }),
+      });
+    }
+  };
 
   return (
     <>
       <Header />
       <S.Conatiner>
         <S.Text>채팅 목록</S.Text>
+        <button onClick={sendMessage}>테스트</button>
       </S.Conatiner>
     </>
   );
