cb8d5424e72f230a675bb1612e1e9d6373cc57ea
hyeongrok7874 <khr5k0228@gmail.com>
2024-04-01 17:51:31 +0900

    Update SelectFormItem description

--- src/components/FormItem/Select/index.tsx
+++ src/components/FormItem/Select/index.tsx
@@ -18,6 +18,7 @@ interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
  * defaultValue 사용법
  * 1. options 배열에 defaultValue를 포함하지 않고 넘긴다.
  * 2. value에 useForm.watch('fieldName')을 넘긴다.
+ * 2-1. react-hook-form 사용 안할 시, value에 상태값을 넘긴다. (제어 컴포넌트)
  *
  * 참고 -  넘겨진 defaultValue는 기본적으로 사용자가 선택할 수 없도록 구현되었다.
  */
