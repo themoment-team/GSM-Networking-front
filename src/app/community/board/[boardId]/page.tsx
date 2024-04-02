e3e0f4d297b0a2bc7e2d87a379d62e3f899b18a7
Lee-Seungje <moondgod@gmail.com>
2024-04-03 08:25:47 +0900

    Add temporaryImgNumber

--- src/app/community/board/[boardId]/page.tsx
+++ src/app/community/board/[boardId]/page.tsx
@@ -1,5 +1,6 @@
 import { getBoardDetail } from '@/apis';
 import { BoardDetail } from '@/pageContainer';
+import type { BoardType } from '@/types';
 
 interface Params {
   params: {
@@ -10,7 +11,27 @@ interface Params {
 const BoardDetailPage: React.FC<Params> = async ({ params: { boardId } }) => {
   const boardDetail = await getBoardDetail('/board', boardId);
 
-  return <BoardDetail initialData={boardDetail} boardId={boardId} />;
+  return (
+    <BoardDetail
+      initialData={addTemporaryImgNumber(boardDetail)}
+      boardId={boardId}
+    />
+  );
 };
 
 export default BoardDetailPage;
+
+const addTemporaryImgNumber = (board: BoardType | null) => {
+  if (!board) return null;
+
+  const newBoard = {
+    ...board,
+    temporaryImgNumber: Math.floor(Math.random() * 5),
+  };
+  newBoard.comments = newBoard.comments.map((comment) => ({
+    ...comment,
+    temporaryImgNumber: Math.floor(Math.random() * 5),
+  }));
+
+  return newBoard;
+};
