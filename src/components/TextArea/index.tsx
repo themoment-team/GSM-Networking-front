a9ec81270c9e9a0f023d84e3980bbee110f11eb3
Lee-Seungje <moondgod@gmail.com>
2024-04-03 00:39:11 +0900

    Add uploadComment

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -14,26 +14,26 @@ interface Props {
   disabled: boolean;
 }
 
-const TextArea: React.FC<Props> = ({ textAreaType, onClick, disabled }) => {
-  const textAreaElements = {
-    gwangya: {
-      placeholder:
-        '비방 및 성적 발언, 욕설 등이 포함된 글은 삭제 조치를 받을 수 있습니다.',
-      icon: <UploadIcon />,
-      MAX_LENGTH: 200,
-    },
-    chatting: {
-      placeholder: '메시지 보내기',
-      icon: <SendIcon />,
-      MAX_LENGTH: 200,
-    },
-    comment: {
-      placeholder: '댓글은 최대 100자 까지 쓸 수 있습니다.',
-      icon: <UploadIcon />,
-      MAX_LENGTH: 300,
-    },
-  } as const;
+const textAreaElements = {
+  gwangya: {
+    placeholder:
+      '비방 및 성적 발언, 욕설 등이 포함된 글은 삭제 조치를 받을 수 있습니다.',
+    icon: <UploadIcon />,
+    MAX_LENGTH: 200,
+  },
+  chatting: {
+    placeholder: '메시지 보내기',
+    icon: <SendIcon />,
+    MAX_LENGTH: 200,
+  },
+  comment: {
+    placeholder: '댓글은 최대 100자 까지 쓸 수 있습니다.',
+    icon: <UploadIcon />,
+    MAX_LENGTH: 300,
+  },
+} as const;
 
+const TextArea: React.FC<Props> = ({ textAreaType, onClick, disabled }) => {
   const [inputValue, setInputValue] = useState<string>('');
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [isFocused, setIsFocused] = useState(false);
