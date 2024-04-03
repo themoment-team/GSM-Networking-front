cffd8f63583ec98edc60d6969f8b1ef209229649
Lee-Seungje <moondgod@gmail.com>
2024-04-03 16:43:28 +0900

    Delete addTemporaryImgNumber

--- src/app/community/board/[boardId]/page.tsx
+++ src/app/community/board/[boardId]/page.tsx
@@ -1,6 +1,5 @@
 import { getBoardDetail } from '@/apis';
 import { BoardDetail } from '@/pageContainer';
-import { addTemporaryImgNumber } from '@/utils';
 
 interface Params {
   params: {
@@ -11,12 +10,7 @@ interface Params {
 const BoardDetailPage: React.FC<Params> = async ({ params: { boardId } }) => {
   const boardDetail = await getBoardDetail('/board', boardId);
 
-  return (
-    <BoardDetail
-      initialData={addTemporaryImgNumber(boardDetail)}
-      boardId={boardId}
-    />
-  );
+  return <BoardDetail initialData={boardDetail} boardId={boardId} />;
 };
 
 export default BoardDetailPage;
