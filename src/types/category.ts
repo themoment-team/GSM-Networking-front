a8461121603e3d824803ba07ae0c32527727ad25
gjaegyun <jaegyungim06@gmail.com>
2024-04-03 17:15:00 +0900

    Change QnA

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,7 +1,7 @@
 export enum CategoryType {
   선생님 = 'TEACHER',
   공지 = 'NOTICE',
-  QNA = 'QNA',
+  QnA = 'QNA',
   잡담 = 'CHAT',
   자료공유 = 'SHARE',
   스터디 = 'STUDY',
@@ -13,7 +13,7 @@ export enum CategoryType {
 export enum ReverseCategoryType {
   TEACHER = '선생님',
   NOTICE = '공지',
-  QNA = 'QNA',
+  QnA = 'QNA',
   CHAT = '잡담',
   SHARE = '자료공유',
   STUDY = '스터디',
