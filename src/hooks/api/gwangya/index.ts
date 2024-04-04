9940aca2c8d66b2c24a94b6d2d167a245904c907
예빈 <yebin060310@gmail.com>
2024-04-04 18:16:32 +0900

    Add hooks

--- src/hooks/api/gwangya/index.ts
+++ src/hooks/api/gwangya/index.ts
@@ -1,3 +1,2 @@
 export * from './useGetGwangyaPostList';
-export * from './useIntersectionObserver';
 export * from './usePostGwanyaContent';
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './api';
 export * from './useAutoResizeTextArea';
 export * from './useForwardRef';
 export * from './useGetRem';
+export * from './useIntersectionObserver';
