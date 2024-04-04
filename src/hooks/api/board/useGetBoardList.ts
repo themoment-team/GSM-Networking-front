61ecfab0969fd0dd2239b888dd1740ffc411ff27
예빈 <yebin060310@gmail.com>
2024-04-04 20:48:27 +0900

    Delete baseUrl

--- src/hooks/api/board/useGetBoardList.ts
+++ src/hooks/api/board/useGetBoardList.ts
@@ -6,9 +6,8 @@ import type { BoardInfo } from '@/types';
 export const useGetBoardList = (initialData?: BoardInfo[]) =>
   useInfiniteQuery({
     queryKey: boardQueryKeys.getBoardList(),
-    queryFn: async ({ pageParam }) => get<BoardInfo[]>(`${boardUrl.getBoardList(pageParam)}`, {
-        baseURL: process.env.BASE_URL,
-      }),
+    queryFn: async ({ pageParam }) =>
+      get<BoardInfo[]>(`${boardUrl.getBoardList(pageParam)}`),
     initialPageParam: 0,
     getPreviousPageParam: (firstPage) => firstPage[0]?.id,
     getNextPageParam: (lastPage) => lastPage[lastPage.length - 1]?.id,
