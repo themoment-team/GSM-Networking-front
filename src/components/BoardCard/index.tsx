a6a8ff9aa29c30cc239e9b3e1549e03ef5e45807
예빈 <yebin060310@gmail.com>
2024-04-12 21:23:24 +0900

    Delete ref

--- src/components/BoardCard/index.tsx
+++ src/components/BoardCard/index.tsx
@@ -1,37 +1,37 @@
 'use client';
 
-import React, { forwardRef } from 'react';
-
 import * as S from './style';
 
 import type { BoardInfoType } from '@/types';
 import { ReverseCategoryType } from '@/types';
 import { parseDateString } from '@/utils';
 
-const BoardCard = forwardRef<HTMLDivElement, BoardInfoType>(
-  ({ id, createdAt, title, authorName, boardCategory }, ref) => {
-    const { monthDay, time } = parseDateString(createdAt);
+const BoardCard: React.FC<BoardInfoType> = ({
+  id,
+  createdAt,
+  title,
+  authorName,
+  boardCategory,
+}) => {
+  const { monthDay, time } = parseDateString(createdAt);
 
-    return (
-      <S.ContentBox href={`/community/board/${id}`}>
-        <S.BoardCard ref={ref}>
-          <S.ContentHeader>
-            <S.HeaderTitle>
-              <S.Name>{authorName}</S.Name>
-              <S.DateAndTime>
-                <S.Date>{monthDay}</S.Date>
-                <S.Time>{time}</S.Time>
-              </S.DateAndTime>
-            </S.HeaderTitle>
-            <S.Tag>#{ReverseCategoryType[boardCategory]}</S.Tag>
-          </S.ContentHeader>
-          <S.Content>{title}</S.Content>
-        </S.BoardCard>
-      </S.ContentBox>
-    );
-  }
-);
-
-BoardCard.displayName = 'BoardCard';
+  return (
+    <S.ContentBox href={`/community/board/${id}`}>
+      <S.BoardCard>
+        <S.ContentHeader>
+          <S.HeaderTitle>
+            <S.Name>{authorName}</S.Name>
+            <S.DateAndTime>
+              <S.Date>{monthDay}</S.Date>
+              <S.Time>{time}</S.Time>
+            </S.DateAndTime>
+          </S.HeaderTitle>
+          <S.Tag>#{ReverseCategoryType[boardCategory]}</S.Tag>
+        </S.ContentHeader>
+        <S.Content>{title}</S.Content>
+      </S.BoardCard>
+    </S.ContentBox>
+  );
+};
 
 export default BoardCard;
