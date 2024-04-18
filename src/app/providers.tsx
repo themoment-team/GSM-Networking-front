c893b065d47497471aa8fee6bd05fbc4b9e899b1
Lee-Seungje <moondgod@gmail.com>
2024-04-18 22:21:15 +0900

    Add delte cookie

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+import { useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 import { ThemeProvider } from '@emotion/react';
 
@@ -8,6 +12,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 
 import QueryClient from '@/app/queryClient';
 import { theme } from '@/styles';
+import { deleteCookie } from '@/utils';
 
 interface Props {
   children: React.ReactNode;
@@ -15,6 +20,13 @@ interface Props {
 
 const Providers: React.FC<Props> = ({ children }) => {
   const [queryClient] = useState(() => QueryClient);
+
+  const path = usePathname();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    if (!path.includes('auth')) deleteCookie('redirect');
+  }, [path, params]);
 
   return (
     <ThemeProvider theme={theme}>
