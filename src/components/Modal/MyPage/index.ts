557385b9793dc9c4c5f115d64ebc83490ad3289a
gaoooon <gaon9749@gmail.com>
2024-04-25 00:08:14 +0900

    Add DefaultProfileSelect Component

--- src/components/Modal/MyPage/index.ts
+++ src/components/Modal/MyPage/index.ts
@@ -1,2 +1,4 @@
+export { default as DefaultProfileSelect } from './DefaultProfileSelect';
 export { default as MyPageModalWrapper } from './ModalWrapper';
+export { default as ProfileChange } from './ProfileChange';
 export { default as ProfileImgRegisterModal } from './ProfileImgRegister';
