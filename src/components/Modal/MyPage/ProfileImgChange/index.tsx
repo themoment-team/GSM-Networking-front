d957037cee653f1945d6c7967582b7e2464d0fd6
gaoooon <gaon9749@gmail.com>
2024-04-28 23:00:27 +0900

    Add ProfileImgChange Component

---
+++ src/components/Modal/MyPage/ProfileImgChange/index.tsx
@@ -0,0 +1,166 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+
+import Cropper from 'react-easy-crop';
+import { toast } from 'react-toastify';
+
+import * as S from './style';
+
+import {
+  useGetMyInfo,
+  useGetMyMenteeInfo,
+  usePostProfileImgUrl,
+  usePostUploadFile,
+} from '@/hooks';
+import type { PostProfileImgType } from '@/types';
+
+interface Props {
+  imgUrl: string;
+  closeModal: () => void;
+}
+
+interface CropType {
+  x: number;
+  y: number;
+}
+
+interface CroppedAreaType extends CropType {
+  width: number;
+  height: number;
+}
+
+const ProfileImgChange = ({ imgUrl, closeModal }: Props) => {
+  const [crop, setCrop] = useState<CropType>({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState<number>(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] =
+    useState<CroppedAreaType | null>(null);
+
+  const { mutate: mutateUploadFile } = usePostUploadFile();
+  const { mutate: mutateProfileImgUrl } = usePostProfileImgUrl();
+  const { refetch: refetchGetMyInfo } = useGetMyInfo();
+  const { refetch: refetchGetMyMenteeInfo } = useGetMyMenteeInfo();
+
+  const onCropComplete = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (_: any, croppedAreaPixel: CroppedAreaType) => {
+      setCroppedAreaPixels(croppedAreaPixel);
+    },
+    []
+  );
+
+  const onCropButtonClick = async () => {
+    const formData = new FormData();
+    const profileImg = await getCroppedImg(imgUrl, croppedAreaPixels!);
+    const convertImg = dataURLtoFile(profileImg, 'croppedImg.png');
+
+    formData.append('file', convertImg);
+
+    mutateUploadFile(formData, {
+      onSuccess: ({ fileUrl }) => {
+        const data: PostProfileImgType = {
+          profileUrl: fileUrl,
+        };
+
+        mutateProfileImgUrl(data, {
+          onSuccess: () => {
+            toast.success('프로필 이미지 업로드에 성공했습니다.');
+            refetchGetMyMenteeInfo();
+            refetchGetMyInfo();
+            closeModal();
+          },
+          onError: () => {
+            toast.error('프로필 이미지 업로드에 실패했습니다.');
+          },
+        });
+      },
+      onError: () => {
+        toast.error('프로필 이미지 업로드에 실패했습니다.');
+      },
+    });
+  };
+
+  const getCroppedImg = async (
+    imageSrc: string,
+    pixelCrop: CroppedAreaType
+  ) => {
+    const image = new Image();
+    image.src = imageSrc;
+    image.setAttribute('crossOrigin', 'anonymous');
+
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    ctx!.translate(image.width / 2, image.height / 2);
+    ctx!.translate(-image.width / 2, -image.height / 2);
+    ctx!.drawImage(image, 0, 0);
+
+    const croppedCanvas = document.createElement('canvas');
+
+    const croppedCtx = croppedCanvas.getContext('2d');
+
+    croppedCanvas.width = pixelCrop.width;
+    croppedCanvas.height = pixelCrop.height;
+
+    croppedCtx!.drawImage(
+      canvas,
+      pixelCrop.x,
+      pixelCrop.y,
+      pixelCrop.width,
+      pixelCrop.height,
+      0,
+      0,
+      pixelCrop.width,
+      pixelCrop.height
+    );
+
+    return croppedCanvas.toDataURL('image/png');
+  };
+
+  const dataURLtoFile = (dataUrl: string, filename: string) => {
+    const arr = dataUrl.split(',');
+    const mime = arr?.[0].match(/:(.*?);/)?.[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  };
+
+  return (
+    <>
+      <S.DescriptionWrapper>
+        <S.Description>5mb 이하의 파일</S.Description>
+      </S.DescriptionWrapper>
+      <S.CropperWrapper>
+        <Cropper
+          image={imgUrl}
+          aspect={47 / 40}
+          showGrid={false}
+          crop={crop}
+          onCropChange={setCrop}
+          zoom={zoom}
+          onZoomChange={setZoom}
+          onCropComplete={onCropComplete}
+          style={{
+            containerStyle: {
+              width: '15.625rem',
+              height: '13.3125rem',
+              position: 'relative',
+              borderRadius: '0.625rem',
+            },
+            cropAreaStyle: { color: 'rgb(0, 0, 0, 0.4)' },
+          }}
+        />
+      </S.CropperWrapper>
+      <S.Button onClick={onCropButtonClick}>확인</S.Button>
+    </>
+  );
+};
+
+export default ProfileImgChange;
