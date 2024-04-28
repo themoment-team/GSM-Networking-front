0f0cf6bad408374f62b053233143f44a30565115
gaoooon <gaon9749@gmail.com>
2024-04-28 23:01:12 +0900

    Delete style

--- src/components/Modal/MyPage/ProfileImgRegister/style.ts
+++ src/components/Modal/MyPage/ProfileImgRegister/style.ts
@@ -23,20 +23,3 @@ export const XButton = styled.button`
     fill: ${({ theme }) => theme.color.grey[400]};
   }
 `;
-
-export const ImgInput = styled.input`
-  display: none;
-`;
-
-export const ImgInputLabel = styled.label`
-  width: 11.75rem;
-  height: 10rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: ${({ theme }) => theme.color.grey[50]};
-  border: 1px dashed ${({ theme }) => theme.color.grey[200]};
-  border-radius: 0.625rem;
-  cursor: pointer;
-  margin: 1rem 0;
-`;
