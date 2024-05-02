4d9a6c3436df7998794b2afc854355144bb466e9
gaoooon <gaon9749@gmail.com>
2024-05-03 01:01:52 +0900

    Add Icon.tsx

---
+++ src/assets/CheckedIcon.tsx
@@ -0,0 +1,19 @@
+const CheckedIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='0.875rem'
+    height='0.875rem'
+    viewBox='0 0 14 14'
+    fill='none'
+  >
+    <rect width='14' height='14' rx='4' fill='#148EFF' />
+    <path
+      d='M3 6.5L5.25915 9.1356C5.65191 9.59381 6.35797 9.60234 6.76169 9.15377L10.5001 5'
+      stroke='white'
+      stroke-width='1.5'
+      stroke-linecap='round'
+    />
+  </svg>
+);
+
+export default CheckedIcon;
