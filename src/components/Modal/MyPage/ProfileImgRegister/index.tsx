dd137eb857db7b010769b869956aac7d321d63c3
방가온 <128475660+gaoooon@users.noreply.github.com>
2024-05-03 12:27:44 +0900

    Delete annotation

--- src/components/Modal/MyPage/ProfileImgRegister/index.tsx
+++ src/components/Modal/MyPage/ProfileImgRegister/index.tsx
@@ -23,48 +23,6 @@ const ProfileImgRegisterModal: React.FC<Props> = ({ closeModal }) => {
   const [step, setStep] = useState<Step>(Step.DEFAULT);
   const [imgUrl, setImgUrl] = useState<string>('');
 
-  // const { mutate: mutateUploadFile } = usePostUploadFile();
-  // const { mutate: mutateProfileImgUrl } = usePostProfileImgUrl();
-  // const { refetch: refetchGetMyInfo } = useGetMyInfo();
-  // const { refetch: refetchGetMyMenteeInfo } = useGetMyMenteeInfo();
-
-  // const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (!e.target.files) return;
-
-  //   const profileImg = e.target.files[0];
-
-  //   if (profileImg.size > 1024 * 1024 * 20) {
-  //     return toast.error('프로필 이미지는 20MB 이하만 업로드 가능합니다.');
-  //   }
-
-  //   const formData = new FormData();
-
-  //   formData.append('file', profileImg);
-
-  //   mutateUploadFile(formData, {
-  //     onSuccess: ({ fileUrl }) => {
-  //       const data: PostProfileImgType = {
-  //         profileUrl: fileUrl,
-  //       };
-
-  //       mutateProfileImgUrl(data, {
-  //         onSuccess: () => {
-  //           toast.success('프로필 이미지 업로드에 성공했습니다.');
-  //           refetchGetMyMenteeInfo();
-  //           refetchGetMyInfo();
-  //           closeModal();
-  //         },
-  //         onError: () => {
-  //           toast.error('프로필 이미지 업로드에 실패했습니다.');
-  //         },
-  //       });
-  //     },
-  //     onError: () => {
-  //       toast.error('프로필 이미지 업로드에 실패했습니다.');
-  //     },
-  //   });
-  // };
-
   return (
     <MyPageModalWrapper
       closeModal={closeModal}
