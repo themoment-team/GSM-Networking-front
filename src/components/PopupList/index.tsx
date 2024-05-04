d730f6dc38192e0a1c23119aa50caa2783676f87
gaoooon <gaon9749@gmail.com>
2024-05-04 22:57:33 +0900

    Add PopupList

---
+++ src/components/PopupList/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { PopupCard } from '@/components';
+import type { PopupType } from '@/types';
+import { getPopupList } from '@/utils';
+
+const PopupList = () => {
+  const [popupList, setPopupList] = useState<PopupType[]>();
+
+  const getPopup = async () => {
+    const response = await getPopupList();
+    // console.log(response);
+    // const filtered = response.filter(() => {});
+    // setPopupList()
+  };
+
+  useEffect(() => {
+    getPopup();
+  }, []);
+
+  return (
+    <>
+      {popupList &&
+        popupList.map((item) => <PopupCard key={item.id} {...item} />)}
+    </>
+  );
+};
+
+export default PopupList;
