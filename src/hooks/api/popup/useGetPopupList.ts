e9521e09a31c360c9268c327505a15e9126cefca
gaoooon <gaon9749@gmail.com>
2024-05-06 05:37:18 +0900

    Update useGetPopupList.ts

--- src/hooks/api/popup/useGetPopupList.ts
+++ src/hooks/api/popup/useGetPopupList.ts
@@ -4,9 +4,14 @@ import { etcQueryKeys, etcUrl, get } from '@/libs';
 import type { PopupType } from '@/types';
 import { minutesToMs } from '@/utils';
 
-export const useGetPopupList = () =>
-  useQuery<PopupType[]>({
+import type { UseQueryOptions } from '@tanstack/react-query';
+
+export const useGetPopupList = (
+  options?: Omit<UseQueryOptions<PopupType[] | null>, 'queryKey'>
+) =>
+  useQuery({
     queryKey: etcQueryKeys.getPopupList(),
     queryFn: () => get<PopupType[]>(etcUrl.getPopupList()),
     staleTime: minutesToMs(3),
+    ...options,
   });
