661f341b540ec21f874e15cf0e7b298cc2daab49
gaoooon <gaon9749@gmail.com>
2024-05-06 05:36:49 +0900

    Update PopupList

--- src/components/PopupList/index.tsx
+++ src/components/PopupList/index.tsx
@@ -1,21 +1,40 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { PopupCardWrapper } from './style';
 
 import { PopupCard } from '@/components';
+import { INVISIBLEPOPUPLIST } from '@/constants';
 import { useGetPopupList } from '@/hooks/api/popup';
+import type { PopupType } from '@/types';
 
 const PopupList = () => {
   const { data } = useGetPopupList();
-  // const filtered = response.filter(() => {});
+  const [popupList, setPopupList] = useState<PopupType[]>([]);
+  const prevInvisiblePopuplist = localStorage.getItem(INVISIBLEPOPUPLIST);
+  const invisiblePopupList = prevInvisiblePopuplist
+    ? (JSON.parse(prevInvisiblePopuplist) as number[])
+    : [];
 
-  useEffect(() => {}, [data]);
+  useEffect(() => {
+    if (data) {
+      const filteredList = data.filter(
+        ({ id }) => !invisiblePopupList.includes(id)
+      );
+      setPopupList(filteredList);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
 
   return (
-    <>
+    <PopupCardWrapper>
       {popupList &&
-        popupList.map((item) => <PopupCard key={item.id} {...item} />)}
-    </>
+        popupList.map((item) => (
+          <PopupCard key={item.id} {...item} setPopupList={setPopupList} />
+        ))}
+    </PopupCardWrapper>
   );
 };
 
---
+++ src/components/PopupList/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const PopupCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.875rem;
+  position: absolute;
+  left: 50%;
+  top: 22.1875rem;
+  /* transform: translateX(-50%); */
+`;
