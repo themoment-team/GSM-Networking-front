cb5102509ae3c849ea57bf4b9ca98b7d8d13d90c
gaoooon <gaon9749@gmail.com>
2024-05-12 01:29:14 +0900

    Update type

--- src/utils/isNumberArray.ts
+++ src/utils/isNumberArray.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const isNumberArray = (item: any): boolean =>
+const isNumberArray = (item: unknown): boolean =>
   Array.isArray(item) && item.every((i) => typeof i === 'number')
     ? true
     : false;
