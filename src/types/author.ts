2908aeeeb024f64222f54a6e3665f5f3d3418805
gaoooon <gaon9749@gmail.com>
2024-06-18 08:42:07 +0900

    Add authorType

---
+++ src/types/author.ts
@@ -0,0 +1,5 @@
+import type { ProfileType } from '.';
+
+export interface AuthorType extends ProfileType {
+  id: number;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export * from './auth';
+export * from './author';
 export * from './board';
 export * from './boardInfo';
 export * from './career';
