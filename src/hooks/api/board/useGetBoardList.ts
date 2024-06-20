bde25db4bdcdb56341abf4769eb7a40cc46de341
gjaegyun <jaegyungim06@gmail.com>
2024-06-20 16:42:51 +0900

    Add category props

--- src/hooks/api/board/useGetBoardList.ts
+++ src/hooks/api/board/useGetBoardList.ts
@@ -1,13 +1,16 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { get, boardQueryKeys, boardUrl } from '@/libs';
-import type { BoardInfoType } from '@/types';
+import type { BoardInfoType, CategoryType } from '@/types';
 
-export const useGetBoardList = (initialData?: BoardInfoType[]) =>
+export const useGetBoardList = (
+  category: CategoryType | null,
+  initialData?: BoardInfoType[]
+) =>
   useInfiniteQuery({
     queryKey: boardQueryKeys.getBoardList(),
     queryFn: async ({ pageParam }) =>
-      get<BoardInfoType[]>(`${boardUrl.getBoardList(pageParam)}`),
+      get<BoardInfoType[]>(`${boardUrl.getBoardList(pageParam, category)}`),
     initialPageParam: 0,
     getPreviousPageParam: (firstPage) => firstPage[0]?.id,
     getNextPageParam: (lastPage) => lastPage[lastPage.length - 1]?.id,
