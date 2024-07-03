8a4a0b6497552aff95798bee01f5e56fc4de209d
Lee-Seungje <moondgod@gmail.com>
2024-07-03 10:02:05 +0900

    feat(채팅): 필요 없는 부분 주석처리

--- src/pageContainer/chat/list/index.tsx
+++ src/pageContainer/chat/list/index.tsx
@@ -1,69 +1,66 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import * as S from './style';
+// import * as S from './style';
 
-// eslint-disable-next-line import/order
-import { Header } from '@/components';
-// import { useSocket } from '@/components';
+// import { Header } from '@/components';
+// import { useWebSocket } from '@/hooks';
 
-import { useWebSocket } from '@/hooks';
+// interface message {
+//   userId: number;
+//   content: string;
+// }
 
-interface message {
-  userId: number;
-  content: string;
-}
+// const ChatList = () => {
+//   const [messages, setMessages] = useState<message[]>([]);
+//   const [currentMessage, setCurrentMessage] = useState('');
+//   // const { socket /*, isConnected*/ } = useSocket();
+//   const [userId /*, setUserId*/] = useState<number>();
 
-const ChatList = () => {
-  const [messages, setMessages] = useState<message[]>([]);
-  const [currentMessage, setCurrentMessage] = useState('');
-  // const { socket /*, isConnected*/ } = useSocket();
-  const [userId /*, setUserId*/] = useState<number>();
+//   const handleWebSocketMessage = (message: string) => {
+//     console.log('Received message:', message);
+//     // setMessages((prevMessages) => [...prevMessages, message]);
+//   };
 
-  const handleWebSocketMessage = (message: string) => {
-    console.log('Received message:', message);
-    // setMessages((prevMessages) => [...prevMessages, message]);
-  };
+//   const sendMessage = useWebSocket(
+//     `${process.env.NEXT_PUBLIC_SOCKET_BASE_URL}`,
+//     handleWebSocketMessage
+//   );
 
-  const sendMessage = useWebSocket(
-    `${process.env.NEXT_PUBLIC_SOCKET_BASE_URL}`,
-    handleWebSocketMessage
-  );
+//   // useEffect(() => {
+//   //   if (!socket) {
+//   //     return;
+//   //   }
 
-  // useEffect(() => {
-  //   if (!socket) {
-  //     return;
-  //   }
+//   //   socket.on('message', (data: message) => {
+//   //     setMessages((messages) => [...messages, ...[data]]);
+//   //   });
 
-  //   socket.on('message', (data: message) => {
-  //     setMessages((messages) => [...messages, ...[data]]);
-  //   });
+//   //   return () => {
+//   //     socket.off('message');
+//   //   };
+//   // }, [socket, messages]);
 
-  //   return () => {
-  //     socket.off('message');
-  //   };
-  // }, [socket, messages]);
+//   // const sendMessage = async (e: React.MouseEvent<HTMLButtonElement>) => {
+//   //   e.preventDefault();
+//   //   await axios.post('/api/chat', {
+//   //     userId: userId,
+//   //     content: currentMessage + '테스트',
+//   //   });
+//   //   setCurrentMessage('');
+//   // };
 
-  // const sendMessage = async (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
-  //   await axios.post('/api/chat', {
-  //     userId: userId,
-  //     content: currentMessage + '테스트',
-  //   });
-  //   setCurrentMessage('');
-  // };
+//   return (
+//     <>
+//       <Header />
+//       <S.Conatiner>
+//         <S.Text>채팅 목록</S.Text>
+//         {chatList?.map((i) => <ChattingListCard key={i.opponentUserId}  />)}
+//         <button onClick={() => sendMessage('test')}>테스트</button>
+//       </S.Conatiner>
+//     </>
+//   );
+// };
 
-  return (
-    <>
-      <Header />
-      <S.Conatiner>
-        <S.Text>채팅 목록</S.Text>
-        {/* {chatList?.map((i) => <ChattingListCard key={i.opponentUserId}  />)} */}
-        <button onClick={() => sendMessage('test')}>테스트</button>
-      </S.Conatiner>
-    </>
-  );
-};
-
-export default ChatList;
+// export default ChatList;
--- src/pageContainer/chat/room/style.ts
+++ src/pageContainer/chat/room/style.ts
@@ -1,28 +1,28 @@
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
 
-export const Container = styled.div`
-  padding-top: 4.375rem;
-  height: 100vh;
-`;
+// export const Container = styled.div`
+//   padding-top: 4.375rem;
+//   height: 100vh;
+// `;
 
-export const PostWrapper = styled.div`
-  height: calc(100% - 5.5rem);
-  display: flex;
-  flex-direction: column;
-`;
+// export const PostWrapper = styled.div`
+//   height: calc(100% - 5.5rem);
+//   display: flex;
+//   flex-direction: column;
+// `;
 
-export const MessageBox = styled.div`
-  /* height: 53rem; */
-  height: 100%;
-  overflow-y: scroll;
+// export const MessageBox = styled.div`
+//   /* height: 53rem; */
+//   height: 100%;
+//   overflow-y: scroll;
 
-  ::-webkit-scrollbar {
-    background: none;
-    width: 0.5rem;
-  }
+//   ::-webkit-scrollbar {
+//     background: none;
+//     width: 0.5rem;
+//   }
 
-  ::-webkit-scrollbar-thumb {
-    background: #d9d9d9;
-    border-radius: 0.625rem;
-  }
-`;
+//   ::-webkit-scrollbar-thumb {
+//     background: #d9d9d9;
+//     border-radius: 0.625rem;
+//   }
+// `;
