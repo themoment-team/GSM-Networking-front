44cbe1eea3d344356c3cd3682a977dc7ea65d07b
Lee-Seungje <moondgod@gmail.com>
2024-07-07 16:43:02 +0900

    refactor: useCheckIsTeacher 내부 불필요한 변수 할당 제거

--- src/hooks/useCheckIsTeacher.ts
+++ src/hooks/useCheckIsTeacher.ts
@@ -3,7 +3,5 @@ import { useGetIsTeacher } from '@/hooks';
 export const useCheckIsTeacher = () => {
   const { data } = useGetIsTeacher();
 
-  const isTeacher = !!data?.isTeacher;
-
-  return isTeacher;
+  return !!data?.isTeacher;
 };
